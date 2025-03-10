ace8e0a0a0fb21560ab7d284c7b6e5ea193fdf53
mthierman <mthierman@gmail.com>
2025-03-10 12:57:15 -0600

    Add packages script

--- packages.ps1
+++ packages.ps1
@@ -1,14 +1,15 @@
-# nuget install "Microsoft.Web.WebView2" -Version "1.0.3065.39" -x -OutputDirectory "packages"
+if (-not (Test-Path "packages/ada-3.2.1.zip"))
+{
+    gh release -R ada-url/ada download -A zip -D "packages"
+    Expand-Archive -Path "packages/ada-3.2.1.zip" -DestinationPath "packages"
+}
 
-# nuget install "Microsoft.Windows.CppWinRT" -Version "2.0.240405.15" -x -OutputDirectory "packages"
+if (-not (Test-Path "packages/glaze-5.0.0.zip"))
+{
+    gh release -R stephenberry/glaze download -A zip -D "packages"
+    Expand-Archive -Path "packages/glaze-5.0.0.zip" -DestinationPath "packages"
+}
 
-# nuget install "Microsoft.Windows.ImplementationLibrary" -Version "1.0.240803.1" -x -OutputDirectory "packages"
-
-# nuget install "${CMAKE_CURRENT_SOURCE_DIR}/packages.config" -x -OutputDirectory "${CMAKE_SOURCE_DIR}/packages"
-
-# "packages/Microsoft.Windows.CppWinRT/bin/cppwinrt" -input sdk -output "${CMAKE_BINARY_DIR}/cppwinrt"
-
-# gh repo clone stephenberry/glaze "${CMAKE_SOURCE_DIR}/packages/glaze"
-
-# git clone --depth=1 --branch=v4.4.3 --recurse-submodules "https://github.com/stephenberry/glaze.git" "packages/glaze"
-
+nuget install "Microsoft.Web.WebView2" -Version "1.0.3124.44" -x -OutputDirectory "packages"
+nuget install "Microsoft.Windows.CppWinRT" -Version "2.0.240405.15" -x -OutputDirectory "packages"
+nuget install "Microsoft.Windows.ImplementationLibrary" -Version "1.0.240803.1" -x -OutputDirectory "packages"
