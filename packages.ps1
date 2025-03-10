6f37c4b8cedae89cefa43502fe1d39ba6dff3b9f
mthierman <mthierman@gmail.com>
2025-03-10 13:04:11 -0600

    Fix packages script

--- packages.ps1
+++ packages.ps1
@@ -1,13 +1,15 @@
-if (-not (Test-Path "packages/ada-3.2.1.zip"))
+if (-not (Test-Path "packages/ada-url/ada"))
 {
-    gh release -R ada-url/ada download -A zip -D "packages"
-    Expand-Archive -Path "packages/ada-3.2.1.zip" -DestinationPath "packages"
+    gh release -R ada-url/ada download -p "singleheader.zip" -D "packages/ada-url/ada"
+    Expand-Archive -Path "packages/ada-url/ada/singleheader.zip" -DestinationPath "packages/ada-url/ada"
+    Remove-Item -Path "packages/ada-url/ada/singleheader.zip"
 }
 
-if (-not (Test-Path "packages/glaze-5.0.0.zip"))
+if (-not (Test-Path "packages/stephenberry/glaze"))
 {
-    gh release -R stephenberry/glaze download -A zip -D "packages"
-    Expand-Archive -Path "packages/glaze-5.0.0.zip" -DestinationPath "packages"
+    gh release -R stephenberry/glaze download -A zip -D "packages/stephenberry/glaze"
+    Expand-Archive -Path "packages/stephenberry/glaze/glaze-5.0.0.zip" -DestinationPath "packages/stephenberry/glaze"
+    Remove-Item -Path "packages/stephenberry/glaze/glaze-5.0.0.zip"
 }
 
 nuget install "Microsoft.Web.WebView2" -Version "1.0.3124.44" -x -OutputDirectory "packages"
