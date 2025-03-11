f4a12d7035101bd68c1ee3cd8b6d71e24d139eca
mthierman <mthierman@gmail.com>
2025-03-11 15:43:43 -0600

    Re-arrange install_packages

--- cmake/install_packages.cmake
+++ cmake/install_packages.cmake
@@ -3,14 +3,6 @@ add_custom_target(
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
     COMMAND ${CMAKE_COMMAND} -E rm -rf "packages"
     COMMAND
-        nuget install "Microsoft.Web.WebView2" -Version "1.0.3065.39" -x -OutputDirectory "packages"
-    COMMAND
-        nuget install "Microsoft.Windows.CppWinRT" -Version "2.0.240405.15" -x -OutputDirectory
-        "packages"
-    COMMAND
-        nuget install "Microsoft.Windows.ImplementationLibrary" -Version "1.0.240803.1" -x
-        -OutputDirectory "packages"
-    COMMAND
         nuget install "${CMAKE_CURRENT_SOURCE_DIR}/packages.config" -x -OutputDirectory
         "${CMAKE_SOURCE_DIR}/packages"
         # COMMAND "${CMAKE_SOURCE_DIR}/packages/Microsoft.Windows.CppWinRT/bin/cppwinrt" -input sdk
@@ -18,4 +10,12 @@ add_custom_target(
         # --recurse-submodules "https://github.com/stephenberry/glaze.git"
         # "${CMAKE_SOURCE_DIR}/packages/glaze"
     COMMAND gh repo clone stephenberry/glaze "${CMAKE_SOURCE_DIR}/packages/glaze"
+    COMMAND
+        nuget install "Microsoft.Web.WebView2" -Version "1.0.3124.44" -x -OutputDirectory "packages"
+    COMMAND
+        nuget install "Microsoft.Windows.CppWinRT" -Version "2.0.240405.15" -x -OutputDirectory
+        "packages"
+    COMMAND
+        nuget install "Microsoft.Windows.ImplementationLibrary" -Version "1.0.240803.1" -x
+        -OutputDirectory "packages"
     )
