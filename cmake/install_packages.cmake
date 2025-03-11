1b0c1c01ed9df9265e24cd1705dc19cafc5e8af0
mthierman <mthierman@gmail.com>
2025-03-11 15:47:51 -0600

    Move scripts into subdir

--- cmake/install_packages.cmake
+++ cmake/install_packages.cmake
@@ -1,4 +1,4 @@
 add_custom_target(
     install_packages WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND pwsh -nol -nop -f "${CMAKE_SOURCE_DIR}/install_packages.ps1"
+    COMMAND pwsh -nol -nop -f "${CMAKE_SOURCE_DIR}/scripts/install_packages.ps1"
     )
