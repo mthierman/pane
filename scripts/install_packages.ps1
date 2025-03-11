189749fd7dbcfe3a1c7099a6ad1ec7e3f3ab261b
mthierman <mthierman@gmail.com>
2025-03-11 15:51:59 -0600

    Use packages.config

--- scripts/install_packages.ps1
+++ scripts/install_packages.ps1
@@ -17,8 +17,6 @@ if (-not (Test-Path "packages/stephenberry/glaze-5.0.0"))
     Remove-Item -Path "packages/stephenberry/glaze-5.0.0.zip"
 }
 
-nuget install "Microsoft.Web.WebView2" -Version "1.0.3124.44" -x -OutputDirectory "packages"
-nuget install "Microsoft.Windows.CppWinRT" -Version "2.0.240405.15" -x -OutputDirectory "packages"
-nuget install "Microsoft.Windows.ImplementationLibrary" -Version "1.0.240803.1" -x -OutputDirectory "packages"
+nuget install -x -OutputDirectory "packages"
 
 Pop-Location
