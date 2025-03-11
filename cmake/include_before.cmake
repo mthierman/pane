e27217f2346517e62c007693389fea739fb05b37
mthierman <mthierman@gmail.com>
2025-03-11 15:56:04 -0600

    Add include_before module

---
+++ cmake/include_before.cmake
@@ -0,0 +1,11 @@
+set(FETCHCONTENT_SOURCE_DIR_PANE_GLAZE "${CMAKE_SOURCE_DIR}/packages/stephenberry/glaze")
+
+set(FETCHCONTENT_SOURCE_DIR_PANE_ADA "${CMAKE_SOURCE_DIR}/packages/ada-url/ada")
+
+set(FETCHCONTENT_SOURCE_DIR_PANE_WEBVIEW2 "${CMAKE_SOURCE_DIR}/packages/Microsoft.Web.WebView2")
+
+set(FETCHCONTENT_SOURCE_DIR_PANE_WIL
+    "${CMAKE_SOURCE_DIR}/packages/Microsoft.Windows.ImplementationLibrary"
+    )
+
+set(FETCHCONTENT_SOURCE_DIR_PANE_CPPWINRT "${CMAKE_SOURCE_DIR}/packages/Microsoft.Windows.CppWinRT")
