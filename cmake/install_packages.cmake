0b8115710c36ae963caf8e8857ee097752bb7324
mthierman <mthierman@gmail.com>
2025-03-11 15:47:23 -0600

    Re-name install_packages script

--- cmake/install_packages.cmake
+++ cmake/install_packages.cmake
@@ -1,4 +1,4 @@
 add_custom_target(
     install_packages WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND pwsh -nol -nop -f "${CMAKE_SOURCE_DIR}/packages.ps1"
+    COMMAND pwsh -nol -nop -f "${CMAKE_SOURCE_DIR}/install_packages.ps1"
     )
