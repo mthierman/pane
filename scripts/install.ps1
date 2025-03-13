5a7a4a7b3edbdf9958b03f07f1c668c609d89467
mthierman <mthierman@gmail.com>
2025-03-13 17:46:27 -0600

    Fix install script to match FetchContent

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,22 +1,46 @@
 Push-Location
 
-Set-Location ($PSScriptRoot | Split-Path)
+$packages = $PSScriptRoot | Split-Path | Join-Path -ChildPath "packages"
 
-if (-not (Test-Path "packages/ada-url/ada"))
+New-Item -ItemType Directory $packages -ErrorAction SilentlyContinue
+Set-Location -Path $packages
+
+$ada_src = "pane_ada-src"
+if (-not (Test-Path "$ada_src"))
 {
-    gh release -R ada-url/ada download "v3.2.1" -p "singleheader.zip" -D "packages/ada-url"
-    Expand-Archive -Path "packages/ada-url/singleheader.zip" -DestinationPath "packages/ada-url/ada"
-    Remove-Item -Path "packages/ada-url/singleheader.zip"
+    gh release -R ada-url/ada download "v3.2.1" -p "singleheader.zip"
+    Expand-Archive -Path "singleheader.zip" -DestinationPath "$ada_src"
+    Remove-Item -Path "singleheader.zip"
 }
 
-if (-not (Test-Path "packages/stephenberry/glaze"))
+$glaze_src = "pane_glaze-src"
+if (-not (Test-Path "$glaze_src"))
 {
-    gh release -R stephenberry/glaze download "v5.0.0" -A zip -D "packages/stephenberry"
-    Expand-Archive -Path "packages/stephenberry/glaze-5.0.0.zip" -DestinationPath "packages/stephenberry"
-    Rename-Item -Path "packages/stephenberry/glaze-5.0.0" -NewName "glaze"
-    Remove-Item -Path "packages/stephenberry/glaze-5.0.0.zip"
+    gh release -R stephenberry/glaze download "v5.0.0" -A zip
+    Expand-Archive -Path "glaze-5.0.0.zip" -DestinationPath "."
+    Rename-Item -Path "glaze-5.0.0" -NewName $glaze_src
+    Remove-Item -Path "glaze-5.0.0.zip"
 }
 
-nuget install -x -OutputDirectory "packages"
+$webview2_src = "pane_webview2-src"
+if (-not (Test-Path "$webview2_src"))
+{
+    nuget install Microsoft.Web.WebView2 -x
+    Rename-Item -Path "Microsoft.Web.WebView2" -NewName $webview2_src
+}
+
+$cppwinrt_src = "pane_cppwinrt-src"
+if (-not (Test-Path "$cppwinrt_src"))
+{
+    nuget install Microsoft.Windows.CppWinRT -x
+    Rename-Item -Path "Microsoft.Windows.CppWinRT" -NewName $cppwinrt_src
+}
+
+$wil_src = "pane_wil-src"
+if (-not (Test-Path "$wil_src"))
+{
+    nuget install Microsoft.Windows.ImplementationLibrary -x
+    Rename-Item -Path "Microsoft.Windows.ImplementationLibrary" -NewName $wil_src
+}
 
 Pop-Location
