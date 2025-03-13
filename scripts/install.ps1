17eb3dda34d5d97f2c5b1f7f27e80f99bb8541a2
mthierman <mthierman@gmail.com>
2025-03-13 14:58:37 -0600

    Version installed packages from GH

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -4,14 +4,14 @@ Set-Location ($PSScriptRoot | Split-Path)
 
 if (-not (Test-Path "packages/ada-url/ada"))
 {
-    gh release -R ada-url/ada download -p "singleheader.zip" -D "packages/ada-url"
+    gh release -R ada-url/ada download "v3.2.1" -p "singleheader.zip" -D "packages/ada-url"
     Expand-Archive -Path "packages/ada-url/singleheader.zip" -DestinationPath "packages/ada-url/ada"
     Remove-Item -Path "packages/ada-url/singleheader.zip"
 }
 
 if (-not (Test-Path "packages/stephenberry/glaze-5.0.0"))
 {
-    gh release -R stephenberry/glaze download -A zip -D "packages/stephenberry"
+    gh release -R stephenberry/glaze download "v5.0.0" -A zip -D "packages/stephenberry"
     Expand-Archive -Path "packages/stephenberry/glaze-5.0.0.zip" -DestinationPath "packages/stephenberry"
     Rename-Item -Path "packages/stephenberry/glaze-5.0.0" -NewName "glaze"
     Remove-Item -Path "packages/stephenberry/glaze-5.0.0.zip"
