212cbe1d3dddfa44efb8c37353a794b7e9b9cb14
mthierman <mthierman@gmail.com>
2025-03-14 01:00:06 -0600

    Rename _install_packages

--- pane/libs/install.cmake
+++ pane/libs/install.cmake
@@ -1,4 +1,4 @@
 add_custom_target(
-    ${PROJECT_NAME}_install WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND pwsh -nol -nop -f "${CMAKE_SOURCE_DIR}/scripts/install.ps1"
+    ${PROJECT_NAME}_install_packages WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+    COMMAND pwsh -nol -nop -f "${PROJECT_NAME}/scripts/install.ps1"
     )
