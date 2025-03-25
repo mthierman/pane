17586140c107c53384dd09d18152d337085f5d80
mthierman <mthierman@gmail.com>
2025-03-24 21:05:24 -0600

    Testing error_code

--- pane/examples/src/console.cxx
+++ pane/examples/src/console.cxx
@@ -1,8 +1,16 @@
 #include <pane/pane.hxx>
+#include <type_traits>
 #include <print>
+#include <system_error>
 
 auto main() -> int {
-    // auto test { pane::temp() };
+    HRESULT hresult { S_OK };
+    // auto error_code { std::error_code(hresult, std::generic_category()) };
+    auto error_code { std::error_code(hresult, std::system_category()) };
+
+    std::println("hresult: {}, message: {}", error_code.value(), error_code.message());
+
+    std::println("hresult: {}, message: {}", hresult, pane::format_message(hresult));
 
     return 0;
 }
