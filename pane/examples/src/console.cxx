ca82f672f8cf7d4acbebc5c53c22924fa1972903
mthierman <mthierman@gmail.com>
2025-03-25 14:52:51 -0600

    Cleanup console example

--- pane/examples/src/console.cxx
+++ pane/examples/src/console.cxx
@@ -4,18 +4,7 @@
 #include <system_error>
 
 auto main() -> int {
-    // auto string { pane::string(u8"test") };
-    // auto path { pane::path(string) };
-
-    // std::println("{}", path.storage);
-
-    HRESULT hresult { S_OK };
-
-    auto code { HRESULT_CODE(hresult) };
-
-    auto err { std::error_code(code, std::system_category()) };
-
-    std::println("{}", err.message());
+    //
 
     return 0;
 }
