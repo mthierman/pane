e105a77eeb488cc61d2937aa7261c2e090ed5749
mthierman <mthierman@gmail.com>
2025-03-25 11:48:36 -0600

    Implement initial path class

--- pane/include/pane/path.hxx
+++ pane/include/pane/path.hxx
@@ -1,7 +1,23 @@
 #pragma once
+#include <string>
+#include <filesystem>
 
 namespace pane {
 struct path {
     using Self = path;
+
+    path() = default;
+    ~path() = default;
+
+    path(Self&& str) noexcept = default;
+    auto operator=(Self&& str) noexcept -> Self& = default;
+
+    path(const Self& str) = default;
+    auto operator=(const Self& str) -> Self& = default;
+
+    explicit path(std::filesystem::path&& path) noexcept;
+    auto operator=(std::filesystem::path&& path) noexcept -> Self&;
+
+    std::filesystem::path storage;
 };
 } // namespace pane
--- pane/src/path.cxx
+++ pane/src/path.cxx
@@ -1,3 +1,12 @@
 #include <pane/path.hxx>
 
-namespace pane { } // namespace pane
+namespace pane {
+path::path(std::filesystem::path&& path) noexcept
+    : storage { std::move(path) } { }
+
+auto path::operator=(std::filesystem::path&& path) noexcept -> Self& {
+    storage = std::move(path);
+
+    return *this;
+}
+} // namespace pane
