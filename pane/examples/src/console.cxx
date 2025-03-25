b3ddc4bf9365a3be42233af55ba8d8c9fb7b19f2
mthierman <mthierman@gmail.com>
2025-03-25 16:00:35 -0600

    Cleanup example

--- pane/examples/src/console.cxx
+++ pane/examples/src/console.cxx
@@ -3,25 +3,10 @@
 #include <print>
 #include <system_error>
 
+#include <comdef.h>
+
 auto main() -> int {
     //
-    // auto app_data { pane::path::from_known_folder() };
-
-    // if (app_data) {
-    //     std::println("{}", (*app_data).storage);
-    // }
-
-    // auto virt { pane::path::from_known_folder(FOLDERID_AddNewPrograms) };
-
-    // if (!virt) {
-    //     std::println("{}", virt.error().message());
-    // }
-
-    auto temp { pane::path::temp_folder() };
-
-    if (temp) {
-        std::println("{}", temp.value().storage);
-    }
 
     return 0;
 }
