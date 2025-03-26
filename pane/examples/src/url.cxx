1c96d24fcf4f871c87e5782cd5afb18060f046ab
mthierman <mthierman@gmail.com>
2025-03-26 14:30:25 -0600

    Print href

--- pane/examples/src/url.cxx
+++ pane/examples/src/url.cxx
@@ -2,11 +2,10 @@
 #include <print>
 
 auto main() -> int {
-    //
     auto url { pane::url::from_string(pane::string("https://www.google.com/")) };
 
     if (url) {
-        std::println("{}", url.value().storage.to_string());
+        std::println("{}", url.value().storage.get_href());
     }
 
     return 0;
