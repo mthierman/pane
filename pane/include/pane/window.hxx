d876a133ae1d0b47aed7aa9d4500e67384061192
mthierman <mthierman@gmail.com>
2025-03-26 21:03:27 -0600

    Remove more :: for window

--- pane/include/pane/window.hxx
+++ pane/include/pane/window.hxx
@@ -9,20 +9,20 @@ struct window {
     window(bool visible = true);
 
     static auto CALLBACK window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
-        -> ::LRESULT;
+        -> LRESULT;
 
-    ::WNDCLASSEXW window_class { .cbSize { sizeof(WNDCLASSEXW) },
-                                 .style { 0 },
-                                 .lpfnWndProc { window_procedure },
-                                 .cbClsExtra { 0 },
-                                 .cbWndExtra { sizeof(Self) },
-                                 .hInstance { pane::module_handle().value_or(nullptr) },
-                                 .hIcon { pane::application_icon() },
-                                 .hCursor { pane::arrow_cursor() },
-                                 .hbrBackground { nullptr },
-                                 .lpszMenuName { nullptr },
-                                 .lpszClassName { L"DefaultWindow" },
-                                 .hIconSm { pane::application_icon() } };
-    ::HWND window_handle;
+    WNDCLASSEXW window_class { .cbSize { sizeof(WNDCLASSEXW) },
+                               .style { 0 },
+                               .lpfnWndProc { window_procedure },
+                               .cbClsExtra { 0 },
+                               .cbWndExtra { sizeof(Self) },
+                               .hInstance { pane::module_handle().value_or(nullptr) },
+                               .hIcon { pane::application_icon() },
+                               .hCursor { pane::arrow_cursor() },
+                               .hbrBackground { nullptr },
+                               .lpszMenuName { nullptr },
+                               .lpszClassName { L"DefaultWindow" },
+                               .hIconSm { pane::application_icon() } };
+    HWND window_handle;
 };
 } // namespace pane
