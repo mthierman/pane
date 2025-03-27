918fa6dcbaa3e0296ad050d6c36bd7f3186f78d2
mthierman <mthierman@gmail.com>
2025-03-26 23:20:38 -0600

    Implement color defaults

--- pane/include/pane/color.hxx
+++ pane/include/pane/color.hxx
@@ -1,3 +1,25 @@
 #pragma once
+#include <Windows.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.ViewManagement.h>
+#include <WebView2.h>
 
-namespace pane { }
+namespace winrt {
+using namespace winrt::Windows::UI;
+using namespace winrt::Windows::UI::ViewManagement;
+}; // namespace winrt
+
+namespace pane {
+struct color {
+    using Self = color;
+
+    color() = default;
+    ~color() = default;
+
+    color(Self&& color) noexcept = default;
+    auto operator=(Self&& color) noexcept -> Self& = default;
+
+    color(const Self& color) = default;
+    auto operator=(const Self& color) -> Self& = default;
+};
+} // namespace pane
