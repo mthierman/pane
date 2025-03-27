842ea364f7c35d3664359e1602a5b93ec3a85562
mthierman <mthierman@gmail.com>
2025-03-26 18:13:43 -0600

    Clean console example

--- pane/examples/src/console.cxx
+++ pane/examples/src/console.cxx
@@ -2,21 +2,7 @@
 #include <print>
 
 auto main() -> int {
-    auto temp_dir { pane::file::from_temp_folder() };
-
-    if (temp_dir) {
-        auto file = pane::file(temp_dir.value());
-        file.storage /= "test.txt";
-        auto create { file.create() };
-
-        std::println("{}, {}", temp_dir.value().storage, create);
-
-        auto open { file.open() };
-
-        if (open) {
-            std::println("{}", file.storage);
-        }
-    }
+    //
 
     return 0;
 }
