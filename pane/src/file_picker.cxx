5d4d108b69de75d04a09bbc8af9582ae55ad223d
mthierman <mthierman@gmail.com>
2025-03-27 15:01:47 -0600

    Cleanup

--- pane/src/file_picker.cxx
+++ pane/src/file_picker.cxx
@@ -10,25 +10,22 @@ namespace pane {
 // https://learn.microsoft.com/en-us/windows/win32/shell/common-file-dialog
 // https://learn.microsoft.com/en-us/windows/win32/shell/library-be-library-aware
 auto file_picker::open(this Self& /* self */) -> void {
-    //
-    auto open_dialog { wil::CoCreateInstance<IFileOpenDialog>(CLSID_FileOpenDialog) };
+    auto dialog { wil::CoCreateInstance<IFileOpenDialog>(CLSID_FileOpenDialog) };
 
-    // FILEOPENDIALOGOPTIONS options;
-    // open_dialog->GetOptions(&options);
-    // open_dialog->SetOptions(options | FOS_PICKFOLDERS | ~FOS_FORCEFILESYSTEM);
+    HRESULT result;
 
     FILEOPENDIALOGOPTIONS options;
-    open_dialog->GetOptions(&options);
-    open_dialog->SetOptions(options | FOS_PICKFOLDERS & ~FOS_FORCEFILESYSTEM);
-
-    // auto result { open_dialog->Show(nullptr) };
-    open_dialog->Show(nullptr);
+    result = dialog->GetOptions(&options);
+    result = dialog->SetOptions(options | FOS_PICKFOLDERS & ~FOS_FORCEFILESYSTEM);
+    result = dialog->Show(nullptr);
 
     wil::com_ptr<IShellItem> item;
-    open_dialog->GetResult(&item);
+    result = dialog->GetResult(&item);
+
     wil::unique_cotaskmem_string buffer;
     // item->GetDisplayName(SIGDN_FILESYSPATH, &buffer);
-    item->GetDisplayName(SIGDN_DESKTOPABSOLUTEPARSING, &buffer);
+    // item->GetDisplayName(SIGDN_DESKTOPABSOLUTEPARSING, &buffer);
+
     OutputDebugStringW(buffer.get());
 
     // auto display_name { pane::string::from_utf16(buffer.get()) };
