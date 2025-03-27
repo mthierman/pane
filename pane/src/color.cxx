a31bf1f6649853586e988516fc95e66f4010188f
mthierman <mthierman@gmail.com>
2025-03-27 01:05:07 -0600

    to_hbrush

--- pane/src/color.cxx
+++ pane/src/color.cxx
@@ -11,4 +11,8 @@ color::color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
 auto color::to_hex(this Self& self) -> string {
     return string(std::format("#{:0>2x}{:0>2x}{:0>2x}{:0>2x}", self.r, self.g, self.b, self.a));
 }
+
+auto color::to_hbrush(this Self& self) -> HBRUSH {
+    return CreateSolidBrush(RGB(self.r, self.g, self.b));
+}
 } // namespace pane
