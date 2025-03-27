79c4cc9e47595be677c9a5ee065535a237f43516
mthierman <mthierman@gmail.com>
2025-03-27 01:26:22 -0600

    to_hex

--- pane/src/color.cxx
+++ pane/src/color.cxx
@@ -8,10 +8,7 @@ color::color(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
       b { b },
       a { a } { }
 
-auto color::to_hex(this Self& self) -> string {
-    // return string(std::format("#{:0>2x}{:0>2x}{:0>2x}{:0>2x}", self.r, self.g, self.b, self.a));
-    return string(std::format("{}", self));
-}
+auto color::to_hex(this Self& self) -> string { return string(std::format("{}", self)); }
 
 auto color::to_colorref(this Self& self) -> COLORREF { return RGB(self.r, self.g, self.b); }
 
