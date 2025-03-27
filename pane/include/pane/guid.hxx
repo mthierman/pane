74e7b5813c28cbaafa041eb933d8d8a49a773650
mthierman <mthierman@gmail.com>
2025-03-26 22:17:46 -0600

    Default ctors for guid

--- pane/include/pane/guid.hxx
+++ pane/include/pane/guid.hxx
@@ -1,3 +1,18 @@
 #pragma once
+#include <Windows.h>
+#include <rpc.h>
 
-namespace pane { }
+namespace pane {
+struct guid {
+    using Self = guid;
+
+    guid() = default;
+    ~guid() = default;
+
+    guid(Self&& file) noexcept = default;
+    auto operator=(Self&& file) noexcept -> Self& = default;
+
+    guid(const Self& file) = default;
+    auto operator=(const Self& file) -> Self& = default;
+};
+} // namespace pane
