1f6820fcf0633c71745f540e0c8b418ef8836479
mthierman <mthierman@gmail.com>
2025-03-28 12:52:34 -0600

    Test guid

--- pane/examples/src/console.cxx
+++ pane/examples/src/console.cxx
@@ -2,8 +2,14 @@
 #include <format>
 #include <print>
 
+#include <pane/guid.hxx>
+
 auto main() -> int {
-    //
+    auto guid { pane::guid::create() };
+
+    if (guid) {
+        std::println("{}", guid.value().to_string().value().get());
+    }
 
     return EXIT_SUCCESS;
 }
