771d60b1eef3c6db60ed0cbc37d07f50f0d61a82
mthierman <mthierman@gmail.com>
2025-03-28 16:18:35 -0600

    Implement text converters

--- pane/include/pane/text.hxx
+++ pane/include/pane/text.hxx
@@ -1,3 +1,8 @@
 #pragma once
+#include <string_view>
+#include <string>
 
-namespace pane { } // namespace pane
+namespace pane {
+auto to_utf16(std::u8string_view string) -> std::u16string;
+auto to_utf8(std::u16string_view string) -> std::u8string;
+} // namespace pane
--- pane/src/text.cxx
+++ pane/src/text.cxx
@@ -1,3 +1,40 @@
 #include <pane/text.hxx>
+#include "icu.hxx"
 
-namespace pane { } // namespace pane
+namespace pane {
+auto to_utf16(std::u8string_view string) -> std::u16string {
+    auto buffer { std::u16string() };
+    auto error_code { U_ZERO_ERROR };
+
+    buffer.resize(string.length());
+
+    u_strFromUTF8WithSub(buffer.data(),
+                         static_cast<int32_t>(buffer.length()),
+                         nullptr,
+                         reinterpret_cast<const char*>(string.data()),
+                         static_cast<int32_t>(string.length()),
+                         0xFFFD,
+                         nullptr,
+                         &error_code);
+
+    return buffer;
+}
+
+auto to_utf8(std::u16string_view string) -> std::u8string {
+    auto buffer { std::u8string() };
+    auto error_code { U_ZERO_ERROR };
+
+    buffer.resize(string.length());
+
+    u_strToUTF8WithSub(reinterpret_cast<char*>(buffer.data()),
+                       static_cast<int32_t>(buffer.length()),
+                       nullptr,
+                       string.data(),
+                       static_cast<int32_t>(string.length()),
+                       0xFFFD,
+                       nullptr,
+                       &error_code);
+
+    return buffer;
+}
+} // namespace pane
