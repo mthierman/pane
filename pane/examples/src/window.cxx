fc143f629855c3ef97ab1649e6c9670098e646a1
mthierman <mthierman@gmail.com>
2025-03-27 20:08:43 -0600

    Test console

--- pane/examples/src/window.cxx
+++ pane/examples/src/window.cxx
@@ -2,8 +2,11 @@
 
 #include <pane/window.hxx>
 #include <pane/system.hxx>
+#include <pane/console.hxx>
 
 auto wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int) -> int {
+    auto console { pane::console() };
+
     auto window { pane::window(true) };
 
     return pane::message_loop();
