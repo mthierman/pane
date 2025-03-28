a3b6f5399292438145e8a7bbd0e49fbcdd26c5d9
mthierman <mthierman@gmail.com>
2025-03-28 17:56:11 -0600

    Fix library example

--- pane/examples/src/library.cxx
+++ pane/examples/src/library.cxx
@@ -8,17 +8,17 @@
 auto main() -> int {
     auto co_init { pane::co_init::multi_threaded() };
 
-    auto samples { pane::file(
+    auto samples { std::filesystem::path(
         L"C:\\Users\\mthie\\AppData\\Roaming\\Microsoft\\Windows\\Libraries\\Samples.library-ms") };
 
-    auto lib { samples.open_library() };
+    auto lib { pane::open_library(samples) };
 
     if (lib) {
-        auto dirs { pane::file::library_directories(lib.value()) };
+        auto dirs { pane::library_directories(lib.value()) };
 
         if (dirs) {
             for (auto& dir : dirs.value()) {
-                std::println("{}", dir.storage);
+                std::println("{}", dir);
             }
         }
     } else {
