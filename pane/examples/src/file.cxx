4cdf8f9f3b70e78a778948aed9d5ede21dc7e245
mthierman <mthierman@gmail.com>
2025-03-28 17:55:17 -0600

    Fix file

--- pane/examples/src/file.cxx
+++ pane/examples/src/file.cxx
@@ -8,19 +8,19 @@
 auto main() -> int {
     auto co_init { pane::co_init::multi_threaded() };
 
-    auto temp_dir { pane::file::from_temp_folder() };
+    auto temp_dir { pane::temp_folder() };
 
     if (temp_dir) {
-        auto file = pane::file(temp_dir.value());
-        file.storage /= "test.txt";
-        auto create { file.create() };
+        // auto file = pane::file(temp_dir.value());
+        temp_dir.value() /= "test.txt";
+        auto create { pane::create_file(temp_dir.value()) };
 
-        std::println("{}, {}", temp_dir.value().storage, create);
+        std::println("{}, {}", temp_dir.value(), create);
 
-        auto open { file.open() };
+        auto open { pane::open_file(temp_dir.value()) };
 
         if (open) {
-            std::println("{}", file.storage);
+            std::println("opened {}", temp_dir.value());
         }
     }
 
