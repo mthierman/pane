f7c698c6d21533f61ed1ffbb1a785847cad59ad0
mthierman <mthierman@gmail.com>
2025-03-28 18:54:17 -0600

    Cleanup console example

--- pane/examples/src/console.cxx
+++ pane/examples/src/console.cxx
@@ -1,15 +1,10 @@
 #include <cstdlib>
 #include <format>
 #include <print>
-
-#include <pane/guid.hxx>
+#include <pane/pane.hxx>
 
 auto main() -> int {
-    auto guid { pane::new_guid() };
-
-    if (guid) {
-        std::println("{}", guid.value());
-    }
+    //
 
     return EXIT_SUCCESS;
 }
