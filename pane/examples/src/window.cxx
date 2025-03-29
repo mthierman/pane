67c1fd0a6de5c8ffa978da6e0f1f621f6ea11d35
mthierman <mthierman@gmail.com>
2025-03-28 20:16:03 -0600

    Fix build

--- pane/examples/src/window.cxx
+++ pane/examples/src/window.cxx
@@ -8,11 +8,11 @@ auto wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int) -> int {
     // pane::message_box(std::string_view(std::format("TEST")), u8"TEST");
     // pane::message_box(std::format("TEST"), std::format("TEST"));
 
-    pane::message_box(u8"TEST", u8"TEST");
-    pane::message_box(std::u8string(u8"TEST"), std::u8string(u8"TEST"));
+    pane::message_box::info(u8"TEST", u8"TEST");
+    pane::message_box::info(std::u8string(u8"TEST"), std::u8string(u8"TEST"));
 
     std::u8string test { u8"TEST" };
-    pane::message_box(test, test);
+    pane::message_box::info(test, test);
 
     return pane::system::message_loop();
 }
