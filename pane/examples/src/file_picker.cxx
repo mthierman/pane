71763539e0cf317dcbced632a813a927e3360a7a
mthierman <mthierman@gmail.com>
2025-03-28 21:44:09 -0600

    Remove window from file_picker example

--- pane/examples/src/file_picker.cxx
+++ pane/examples/src/file_picker.cxx
@@ -3,8 +3,6 @@
 auto wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int) -> int {
     auto co_init { pane::co_init::apartment_threaded() };
 
-    auto window { pane::window(true) };
-
     auto file_picker { pane::file_picker() };
 
     auto dir { file_picker.open_directory() };
