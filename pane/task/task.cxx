1cfb0d57476be737286a6ea9707b3bda741de919
mthierman <mthierman@gmail.com>
2025-03-29 01:15:46 -0600

    color task

--- pane/task/task.cxx
+++ pane/task/task.cxx
@@ -3,20 +3,20 @@
 #include <format>
 #include <print>
 #include <ranges>
+#include <string_view>
 
-// https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
 auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
     auto args { pane::system::command_line_arguments() };
 
-    for (auto [idx, arg] : std::views::enumerate(args) | std::views::as_const) {
-        std::println("{}: {}", idx, std::format("{}", arg));
+    auto task = [&](std::u8string_view cmp) -> bool { return args.at(1).compare(cmp) == 0; };
+
+    if (task(u8"color")) {
+        auto color { pane::color(0, 255, 0) };
+
+        std::println("{}", color);
     }
 
-    if (args[1].compare(u8"color") == 0) {
-        std::println("color");
-    }
-
-    if (args[1].compare(u8"config") == 0) {
+    if (task(u8"config")) {
         std::println("config");
     }
 
