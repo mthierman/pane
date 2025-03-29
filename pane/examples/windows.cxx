7946ac36e0db50f6ce45cd908e5d9d9f0996ffc1
mthierman <mthierman@gmail.com>
2025-03-28 23:22:36 -0600

    Set param names for wWinMain

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -1,7 +1,10 @@
 #include <pane/pane.hxx>
 
 // https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
-auto wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int) -> int {
+auto wWinMain(HINSTANCE /* hinstance */,
+              HINSTANCE /* hprevinstance */,
+              PWSTR /* pcmdline */,
+              int /* ncmdshow */) -> int {
     auto window { pane::window(true) };
 
     return pane::system::message_loop();
