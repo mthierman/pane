9f3173f19e2d693c7978a1f06118aa73a3f4c051
mthierman <mthierman@gmail.com>
2025-03-28 23:21:29 -0600

    Comment entry points

--- pane/examples/console.cxx
+++ pane/examples/console.cxx
@@ -3,6 +3,7 @@
 #include <format>
 #include <print>
 
+// https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
 auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
     std::println("SUBSYSTEM:CONSOLE");
 
--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -1,5 +1,6 @@
 #include <pane/pane.hxx>
 
+// https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
 auto wWinMain(HINSTANCE, HINSTANCE, wchar_t*, int) -> int {
     auto window { pane::window(true) };
 
