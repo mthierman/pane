5ab187871880e55e7eeb37687230721daaf8d79d
mthierman <mthierman@gmail.com>
2025-03-29 00:30:48 -0600

    use ranges to enumerate args

--- pane/examples/console.cxx
+++ pane/examples/console.cxx
@@ -2,14 +2,14 @@
 #include <cstdlib>
 #include <format>
 #include <print>
+#include <ranges>
 
 // https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
 auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
     auto args { pane::system::command_line_arguments() };
 
-    for (auto idx = 0; auto& arg : args) {
+    for (auto [idx, arg] : std::views::enumerate(args) | std::views::as_const) {
         std::println("{}: {}", idx, std::format("{}", arg));
-        ++idx;
     }
 
     return EXIT_SUCCESS;
