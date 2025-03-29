fd14b57e857fb248e9544f1d7f2f22d46c1c786e
mthierman <mthierman@gmail.com>
2025-03-28 18:38:40 -0600

    Cleanup message_box variables

--- pane/src/message_box.cxx
+++ pane/src/message_box.cxx
@@ -4,22 +4,16 @@
 
 namespace pane {
 auto message_box(std::u8string message, std::u8string title) -> int {
-    auto converted_message { pane::to_utf16(message) };
-    auto converted_title { pane::to_utf16(title) };
-
     return MessageBoxW(nullptr,
-                       reinterpret_cast<const wchar_t*>(converted_message.data()),
-                       reinterpret_cast<const wchar_t*>(converted_title.data()),
+                       reinterpret_cast<const wchar_t*>(pane::to_utf16(message).data()),
+                       reinterpret_cast<const wchar_t*>(pane::to_utf16(title).data()),
                        MB_OK | MB_ICONASTERISK);
 }
 
 auto error_box(std::u8string message, std::u8string title) -> int {
-    auto converted_message { pane::to_utf16(message) };
-    auto converted_title { pane::to_utf16(title) };
-
     return MessageBoxW(nullptr,
-                       reinterpret_cast<const wchar_t*>(converted_message.data()),
-                       reinterpret_cast<const wchar_t*>(converted_title.data()),
+                       reinterpret_cast<const wchar_t*>(pane::to_utf16(message).data()),
+                       reinterpret_cast<const wchar_t*>(pane::to_utf16(title).data()),
                        MB_OK | MB_ICONHAND);
 }
 } // namespace pane
