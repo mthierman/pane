4915a641a8970e04294da7e7acdbe02e27a69158
mthierman <mthierman@gmail.com>
2025-03-28 19:10:37 -0600

    Reinstate examples

--- pane/examples/src/library.cxx
+++ pane/examples/src/library.cxx
@@ -1,28 +1,27 @@
 #include <cstdlib>
-// #include <format>
-// #include <print>
-// #include <pane/pane.hxx>
+#include <format>
+#include <print>
+#include <pane/pane.hxx>
 
 auto main() -> int {
-    //     auto co_init { pane::co_init::multi_threaded() };
+    auto co_init { pane::co_init::multi_threaded() };
 
-    //     auto samples { std::filesystem::path(
-    //         L"C:\\Users\\mthie\\AppData\\Roaming\\Microsoft\\Windows\\Libraries\\Samples.library-ms")
-    //         };
+    auto samples { std::filesystem::path(
+        L"C:\\Users\\mthie\\AppData\\Roaming\\Microsoft\\Windows\\Libraries\\Samples.library-ms") };
 
-    //     auto lib { pane::filesystem::open_library(samples) };
+    auto lib { pane::filesystem::open_library(samples) };
 
-    //     if (lib) {
-    //         auto dirs { pane::filesystem::library_directories(lib.value()) };
+    if (lib) {
+        auto dirs { pane::filesystem::library_directories(lib.value()) };
 
-    //         if (dirs) {
-    //             for (auto& dir : dirs.value()) {
-    //                 std::println("{}", dir);
-    //             }
-    //         }
-    //     } else {
-    //         std::println("{}", lib.error().message());
-    //     }
+        if (dirs) {
+            for (auto& dir : dirs.value()) {
+                std::println("{}", dir);
+            }
+        }
+    } else {
+        std::println("{}", lib.error().message());
+    }
 
     return EXIT_SUCCESS;
 }
