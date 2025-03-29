16cce0a8cdee512daa662497c6e58b122a6680ac
mthierman <mthierman@gmail.com>
2025-03-28 23:44:55 -0600

    Args testing

--- pane/examples/console.cxx
+++ pane/examples/console.cxx
@@ -5,11 +5,11 @@
 
 // https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
 auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
-    std::println("SUBSYSTEM:CONSOLE");
     auto args { pane::system::command_line_arguments() };
 
-    for (auto& arg : args) {
-        std::println("{}", std::format("{}", arg));
+    for (auto idx = 0; auto& arg : args) {
+        std::println("{}: {}", idx, std::format("{}", arg));
+        ++idx;
     }
 
     return EXIT_SUCCESS;
