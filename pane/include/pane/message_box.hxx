40a60e72cdb1781f008047383717aac927a6ca9a
mthierman <mthierman@gmail.com>
2025-04-05 13:54:52 -0600

    Switch to string_view for message box

--- pane/include/pane/message_box.hxx
+++ pane/include/pane/message_box.hxx
@@ -2,6 +2,6 @@
 #include <string>
 
 namespace pane::message_box {
-auto info(std::u8string message, std::u8string title = u8"Info") -> int;
-auto error(std::u8string message, std::u8string title = u8"Error") -> int;
+auto info(std::u8string_view message, std::u8string_view title = u8"Info") -> int;
+auto error(std::u8string_view message, std::u8string_view title = u8"Error") -> int;
 } // namespace pane::message_box
--- pane/src/message_box.cxx
+++ pane/src/message_box.cxx
@@ -3,14 +3,14 @@
 #include <pane/text.hxx>
 
 namespace pane::message_box {
-auto info(std::u8string message, std::u8string title) -> int {
+auto info(std::u8string_view message, std::u8string_view title) -> int {
     return MessageBoxW(nullptr,
                        reinterpret_cast<const wchar_t*>(pane::to_utf16(message).data()),
                        reinterpret_cast<const wchar_t*>(pane::to_utf16(title).data()),
                        MB_OK | MB_ICONASTERISK);
 }
 
-auto error(std::u8string message, std::u8string title) -> int {
+auto error(std::u8string_view message, std::u8string_view title) -> int {
     return MessageBoxW(nullptr,
                        reinterpret_cast<const wchar_t*>(pane::to_utf16(message).data()),
                        reinterpret_cast<const wchar_t*>(pane::to_utf16(title).data()),
