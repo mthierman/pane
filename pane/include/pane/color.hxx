c010ca355abb1edda0c3b6bcdfb62660f436cb25
mthierman <mthierman@gmail.com>
2025-04-05 16:31:58 -0600

    Update color member functions

--- pane/include/pane/color.hxx
+++ pane/include/pane/color.hxx
@@ -6,6 +6,7 @@
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.UI.ViewManagement.h>
 #include <WebView2.h>
+#include <pane/system.hxx>
 
 namespace pane {
 struct color final {
@@ -22,28 +23,33 @@ struct color final {
     explicit color(uint8_t r,
                    uint8_t g,
                    uint8_t b,
-                   uint8_t a = std::numeric_limits<uint8_t>::max());
+                   uint8_t alpha = std::numeric_limits<uint8_t>::max());
 
-    explicit color(const COLORREF& colorref, uint8_t a = std::numeric_limits<uint8_t>::max());
+    explicit color(const COLORREF& colorref, uint8_t alpha = std::numeric_limits<uint8_t>::max());
 
     explicit color(const winrt::Windows::UI::Color& winrt_color);
-    explicit color(const winrt::Windows::UI::Color& winrt_color, uint8_t a);
+    explicit color(const winrt::Windows::UI::Color& winrt_color, uint8_t alpha);
+
+    explicit color(winrt::Windows::UI::ViewManagement::UIColorType winrt_ui_color_type);
+    explicit color(winrt::Windows::UI::ViewManagement::UIColorType winrt_ui_color_type,
+                   uint8_t alpha);
 
     explicit color(const COREWEBVIEW2_COLOR& webview2_color);
-    explicit color(const COREWEBVIEW2_COLOR& webview2_color, uint8_t a);
+    explicit color(const COREWEBVIEW2_COLOR& webview2_color, uint8_t alpha);
 
-    auto to_hex(this Self& self) -> std::u8string;
-    auto to_colorref(this Self& self) -> COLORREF;
-    auto to_hbrush(this Self& self) -> HBRUSH;
-    auto to_winrt_color(this Self& self) -> winrt::Windows::UI::Color;
-    auto to_webview2_color(this Self& self) -> COREWEBVIEW2_COLOR;
+    auto to_hex(this const Self& self) -> std::u8string;
+    auto to_colorref(this const Self& self) -> COLORREF;
+    auto to_hbrush(this const Self& self) -> HBRUSH;
+    auto to_winrt_color(this const Self& self) -> winrt::Windows::UI::Color;
+    auto to_webview2_color(this const Self& self) -> COREWEBVIEW2_COLOR;
 
-    auto is_dark(this Self& self) -> bool;
+    auto is_dark(this const Self& self) -> bool;
 
     uint8_t r { std::numeric_limits<uint8_t>::max() };
     uint8_t g { std::numeric_limits<uint8_t>::max() };
     uint8_t b { std::numeric_limits<uint8_t>::max() };
     uint8_t a { std::numeric_limits<uint8_t>::max() };
+    winrt::Windows::UI::ViewManagement::UISettings ui_settings { pane::system::ui_settings() };
 };
 } // namespace pane
 
