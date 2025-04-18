8a86489e5852e88faf53548aa4de1a5fc29b4c77
mthierman <mthierman@gmail.com>
2025-04-18 02:26:07 -0600

    Un-nest window/webview configs to deal with bug in clang

--- pane/include/pane/window.hxx
+++ pane/include/pane/window.hxx
@@ -11,97 +11,97 @@
 #include <WebView2EnvironmentOptions.h>
 
 namespace pane {
+struct window_config final {
+    std::u8string title;
+    pane::color background_color;
+    bool visible { false };
+    bool webview { false };
+    bool shutdown { false };
+};
+
+struct webview_config {
+    struct environment_options final {
+        std::u8string AdditionalBrowserArguments;
+        bool AllowSingleSignOnUsingOSPrimaryAccount { false };
+        std::u8string Language;
+        std::u8string TargetCompatibleBrowserVersion;
+        bool ExclusiveUserDataFolderAccess { false };
+        bool IsCustomCrashReportingEnabled { false };
+        bool EnableTrackingPrevention { true };
+        bool AreBrowserExtensionsEnabled { false };
+        COREWEBVIEW2_CHANNEL_SEARCH_KIND ChannelSearchKind {
+            COREWEBVIEW2_CHANNEL_SEARCH_KIND::COREWEBVIEW2_CHANNEL_SEARCH_KIND_MOST_STABLE
+        };
+        COREWEBVIEW2_SCROLLBAR_STYLE ScrollBarStyle {
+            COREWEBVIEW2_SCROLLBAR_STYLE::COREWEBVIEW2_SCROLLBAR_STYLE_DEFAULT
+        };
+    };
+
+    struct settings final {
+        bool AreBrowserAcceleratorKeysEnabled { true };
+        bool AreDefaultContextMenusEnabled { true };
+        bool AreDefaultScriptDialogsEnabled { true };
+        bool AreDevToolsEnabled { true };
+        bool AreHostObjectsAllowed { true };
+        COREWEBVIEW2_PDF_TOOLBAR_ITEMS HiddenPdfToolbarItems {
+            COREWEBVIEW2_PDF_TOOLBAR_ITEMS::COREWEBVIEW2_PDF_TOOLBAR_ITEMS_NONE
+        };
+        bool IsBuiltInErrorPageEnabled { true };
+        bool IsGeneralAutofillEnabled { true };
+        bool IsNonClientRegionSupportEnabled { true };
+        bool IsPasswordAutosaveEnabled { true };
+        bool IsPinchZoomEnabled { true };
+        bool IsReputationCheckingRequired { true };
+        bool IsScriptEnabled { true };
+        bool IsStatusBarEnabled { true };
+        bool IsSwipeNavigationEnabled { true };
+        bool IsWebMessageEnabled { true };
+        bool IsZoomControlEnabled { true };
+    };
+
+    environment_options environment_options;
+    settings settings;
+    std::filesystem::path browser_executable_folder;
+    std::filesystem::path user_data_folder;
+    std::u8string home_page { u8"about:blank" };
+};
+
+struct webview final {
+    webview_config config;
+    wil::com_ptr<ICoreWebView2Settings9> settings;
+    wil::com_ptr<ICoreWebView2Environment13> environment;
+    wil::com_ptr<ICoreWebView2Controller4> controller;
+    wil::com_ptr<ICoreWebView2_22> core;
+    wil::com_ptr<ICoreWebView2EnvironmentOptions> options {
+        Microsoft::WRL::Make<CoreWebView2EnvironmentOptions>()
+    };
+    wil::com_ptr<ICoreWebView2EnvironmentOptions2> options2 {
+        options.try_query<ICoreWebView2EnvironmentOptions2>()
+    };
+    wil::com_ptr<ICoreWebView2EnvironmentOptions3> options3 {
+        options.try_query<ICoreWebView2EnvironmentOptions3>()
+    };
+    wil::com_ptr<ICoreWebView2EnvironmentOptions4> options4 {
+        options.try_query<ICoreWebView2EnvironmentOptions4>()
+    };
+    wil::com_ptr<ICoreWebView2EnvironmentOptions5> options5 {
+        options.try_query<ICoreWebView2EnvironmentOptions5>()
+    };
+    wil::com_ptr<ICoreWebView2EnvironmentOptions6> options6 {
+        options.try_query<ICoreWebView2EnvironmentOptions6>()
+    };
+    wil::com_ptr<ICoreWebView2EnvironmentOptions7> options7 {
+        options.try_query<ICoreWebView2EnvironmentOptions7>()
+    };
+    wil::com_ptr<ICoreWebView2EnvironmentOptions8> options8 {
+        options.try_query<ICoreWebView2EnvironmentOptions8>()
+    };
+};
+
 struct window final {
     using Self = window;
 
-    struct config final {
-        std::u8string title;
-        pane::color background_color;
-        bool visible { false };
-        bool webview { false };
-        bool shutdown { false };
-    };
-
-    struct webview final {
-        struct config {
-            struct environment_options final {
-                std::u8string AdditionalBrowserArguments;
-                bool AllowSingleSignOnUsingOSPrimaryAccount { false };
-                std::u8string Language;
-                std::u8string TargetCompatibleBrowserVersion;
-                bool ExclusiveUserDataFolderAccess { false };
-                bool IsCustomCrashReportingEnabled { false };
-                bool EnableTrackingPrevention { true };
-                bool AreBrowserExtensionsEnabled { false };
-                COREWEBVIEW2_CHANNEL_SEARCH_KIND ChannelSearchKind {
-                    COREWEBVIEW2_CHANNEL_SEARCH_KIND::COREWEBVIEW2_CHANNEL_SEARCH_KIND_MOST_STABLE
-                };
-                COREWEBVIEW2_SCROLLBAR_STYLE ScrollBarStyle {
-                    COREWEBVIEW2_SCROLLBAR_STYLE::COREWEBVIEW2_SCROLLBAR_STYLE_DEFAULT
-                };
-            };
-
-            struct settings final {
-                bool AreBrowserAcceleratorKeysEnabled { true };
-                bool AreDefaultContextMenusEnabled { true };
-                bool AreDefaultScriptDialogsEnabled { true };
-                bool AreDevToolsEnabled { true };
-                bool AreHostObjectsAllowed { true };
-                COREWEBVIEW2_PDF_TOOLBAR_ITEMS HiddenPdfToolbarItems {
-                    COREWEBVIEW2_PDF_TOOLBAR_ITEMS::COREWEBVIEW2_PDF_TOOLBAR_ITEMS_NONE
-                };
-                bool IsBuiltInErrorPageEnabled { true };
-                bool IsGeneralAutofillEnabled { true };
-                bool IsNonClientRegionSupportEnabled { true };
-                bool IsPasswordAutosaveEnabled { true };
-                bool IsPinchZoomEnabled { true };
-                bool IsReputationCheckingRequired { true };
-                bool IsScriptEnabled { true };
-                bool IsStatusBarEnabled { true };
-                bool IsSwipeNavigationEnabled { true };
-                bool IsWebMessageEnabled { true };
-                bool IsZoomControlEnabled { true };
-            };
-
-            environment_options environment_options;
-            settings settings;
-            std::filesystem::path browser_executable_folder;
-            std::filesystem::path user_data_folder;
-            std::u8string home_page { u8"about:blank" };
-        };
-
-        config config;
-        wil::com_ptr<ICoreWebView2Settings9> settings;
-        wil::com_ptr<ICoreWebView2Environment13> environment;
-        wil::com_ptr<ICoreWebView2Controller4> controller;
-        wil::com_ptr<ICoreWebView2_22> core;
-        wil::com_ptr<ICoreWebView2EnvironmentOptions> options {
-            Microsoft::WRL::Make<CoreWebView2EnvironmentOptions>()
-        };
-        wil::com_ptr<ICoreWebView2EnvironmentOptions2> options2 {
-            options.try_query<ICoreWebView2EnvironmentOptions2>()
-        };
-        wil::com_ptr<ICoreWebView2EnvironmentOptions3> options3 {
-            options.try_query<ICoreWebView2EnvironmentOptions3>()
-        };
-        wil::com_ptr<ICoreWebView2EnvironmentOptions4> options4 {
-            options.try_query<ICoreWebView2EnvironmentOptions4>()
-        };
-        wil::com_ptr<ICoreWebView2EnvironmentOptions5> options5 {
-            options.try_query<ICoreWebView2EnvironmentOptions5>()
-        };
-        wil::com_ptr<ICoreWebView2EnvironmentOptions6> options6 {
-            options.try_query<ICoreWebView2EnvironmentOptions6>()
-        };
-        wil::com_ptr<ICoreWebView2EnvironmentOptions7> options7 {
-            options.try_query<ICoreWebView2EnvironmentOptions7>()
-        };
-        wil::com_ptr<ICoreWebView2EnvironmentOptions8> options8 {
-            options.try_query<ICoreWebView2EnvironmentOptions8>()
-        };
-    };
-
-    window(pane::window::config&& window_config = pane::window::config {},
+    window(pane::window_config&& window_config = {},
            std::function<LRESULT(HWND, UINT, WPARAM, LPARAM)>&& procedure
            = [](HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
                  return DefWindowProcW(hwnd, msg, wparam, lparam);
@@ -121,7 +121,7 @@ private:
     static auto class_window_procedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
         -> LRESULT;
 
-    config window_config;
+    pane::window_config window_config;
     HWND window_handle;
     WNDCLASSEXW window_class {
         .cbSize { sizeof(WNDCLASSEXW) },
@@ -138,6 +138,6 @@ private:
         .hIconSm { pane::system::resource_icon().value_or(pane::system::application_icon()) }
     };
     std::function<LRESULT(HWND, UINT, WPARAM, LPARAM)> window_procedure;
-    webview webview;
+    pane::webview webview;
 };
 } // namespace pane
