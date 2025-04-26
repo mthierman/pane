4d380e5c6c51473f9d238d70af5515f04c18fe43
mthierman <mthierman@gmail.com>
2025-04-26 14:16:28 -0600

    make_window/make_webview

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -1,33 +1,36 @@
 #include <pane/pane.hxx>
 
+auto make_window() -> pane::window {
+    return pane::window({ u8"pane", pane::color { 0, 0, 0, 0 }, true, true },
+                        [&](pane::window::message message) -> LRESULT {
+        if (message.msg == WM_CREATE) {
+            pane::debug("WM_CREATE");
+        }
+
+        return DefWindowProcW(message.hwnd, message.msg, message.wparam, message.lparam);
+    });
+}
+
+auto make_webview() -> pane::webview {
+    return pane::webview({},
+                         { .home_page = u8"https://www.google.com/" },
+                         [&](pane::window::message message) -> LRESULT {
+        if (message.msg == WM_CREATE) {
+            pane::debug("WM_CREATE");
+        }
+
+        return DefWindowProcW(message.hwnd, message.msg, message.wparam, message.lparam);
+    });
+}
+
 // https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
 auto WINAPI wWinMain(HINSTANCE /* hinstance */,
                      HINSTANCE /* hprevinstance */,
                      PWSTR /* pcmdline */,
                      int /* ncmdshow */) -> int {
-    // auto window { pane::window() };
-    // window.~window();
-
-    // auto win { std::make_unique<pane::window>() };
-
-    auto window { pane::window({ u8"pane", pane::color { 0, 0, 0, 0 }, true, true },
-                               [&](pane::window::message message) -> LRESULT {
-        if (message.msg == WM_WINDOWPOSCHANGED) {
-            pane::debug("WM_WINDOWPOSCHANGED");
-        }
-
-        return DefWindowProcW(message.hwnd, message.msg, message.wparam, message.lparam);
-    }) };
-
-    auto webview { pane::webview({},
-                                 { .home_page = u8"https://www.google.com/" },
-                                 [&](pane::window::message message) -> LRESULT {
-        if (message.msg == WM_WINDOWPOSCHANGED) {
-            pane::debug("WM_WINDOWPOSCHANGED");
-        }
-
-        return DefWindowProcW(message.hwnd, message.msg, message.wparam, message.lparam);
-    }) };
+    auto window { make_window() };
+    auto window2 { make_window() };
+    auto webview { make_webview() };
 
     return pane::system::message_loop();
 }
