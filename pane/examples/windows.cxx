161d1bcea789e74ebde7c010123b0441a4c5b912
mthierman <mthierman@gmail.com>
2025-04-26 14:09:36 -0600

    Consolidate windows example

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -5,8 +5,6 @@ auto WINAPI wWinMain(HINSTANCE /* hinstance */,
                      HINSTANCE /* hprevinstance */,
                      PWSTR /* pcmdline */,
                      int /* ncmdshow */) -> int {
-    // auto webview { pane::webview({}, { .home_page = u8"https://www.google.com/" }) };
-
     // auto window { pane::window() };
     // window.~window();
 
@@ -14,9 +12,18 @@ auto WINAPI wWinMain(HINSTANCE /* hinstance */,
 
     auto window { pane::window({ u8"pane", pane::color { 0, 0, 0, 0 }, true, true },
                                [&](pane::window::message message) -> LRESULT {
-        if (message.msg == WM_SETTINGCHANGE) {
-            //
-            // auto rect { message.window->client_rect };
+        if (message.msg == WM_WINDOWPOSCHANGED) {
+            pane::debug("WM_WINDOWPOSCHANGED");
+        }
+
+        return DefWindowProcW(message.hwnd, message.msg, message.wparam, message.lparam);
+    }) };
+
+    auto webview { pane::webview({},
+                                 { .home_page = u8"https://www.google.com/" },
+                                 [&](pane::window::message message) -> LRESULT {
+        if (message.msg == WM_WINDOWPOSCHANGED) {
+            pane::debug("WM_WINDOWPOSCHANGED");
         }
 
         return DefWindowProcW(message.hwnd, message.msg, message.wparam, message.lparam);
