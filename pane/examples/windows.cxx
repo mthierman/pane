0a30b5bf0044fe964dc69f2b8c8705dfbbd56199
mthierman <mthierman@gmail.com>
2025-04-26 14:51:49 -0600

    Test bg colors

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -1,7 +1,7 @@
 #include <pane/pane.hxx>
 
 auto make_window(pane::window_manager& window_manager) -> pane::window {
-    return pane::window({ u8"pane", pane::color { 0, 34, 255, 255 }, true, false },
+    return pane::window({ u8"pane", pane::color { 255, 0, 0, 255 }, true, false },
                         [&](pane::window::message message) -> LRESULT {
         if (message.msg == WM_CREATE) {
             window_manager.insert(message.hwnd);
@@ -16,7 +16,7 @@ auto make_window(pane::window_manager& window_manager) -> pane::window {
 }
 
 auto make_webview(pane::window_manager& window_manager) -> pane::webview {
-    return pane::webview({},
+    return pane::webview({ .background_color = pane::color { 0, 0, 255, 255 } },
                          { .home_page = u8"https://www.google.com/" },
                          [&](pane::window::message message) -> LRESULT {
         if (message.msg == WM_CREATE) {
