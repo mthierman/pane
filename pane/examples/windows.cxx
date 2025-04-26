27667e87fe65f684472f019b24a74945823c247f
mthierman <mthierman@gmail.com>
2025-04-26 17:56:39 -0600

    Webview example working

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -16,21 +16,22 @@ auto make_window(pane::window_manager& window_manager) -> pane::window {
     });
 }
 
-// auto make_webview(pane::window_manager& window_manager) -> pane::webview {
-//     return pane::webview({ .background_color = pane::color { 0, 0, 255, 255 } },
-//                          { .home_page = u8"https://www.google.com/" },
-//                          [&](pane::window_message message) -> LRESULT {
-//         if (message.msg == WM_CREATE) {
-//             window_manager.insert(message.hwnd);
-//         }
+auto make_webview(pane::window_manager& window_manager) -> pane::webview {
+    return pane::webview({ .background_color = pane::color { 0, 0, 255, 255 } },
+                         { .home_page = u8"https://www.google.com/" },
+                         [&](pane::webview::message message) -> LRESULT {
+        if (message.msg == WM_CREATE) {
+            window_manager.insert(message.webview->window_handle);
+        }
 
-//         if (message.msg == WM_DESTROY) {
-//             window_manager.erase(message.hwnd);
-//         }
+        if (message.msg == WM_DESTROY) {
+            window_manager.erase(message.webview->window_handle);
+        }
 
-//         return pane::window::default_procedure(message);
-//     });
-// }
+        return DefWindowProcW(
+            message.webview->window_handle, message.msg, message.wparam, message.lparam);
+    });
+}
 
 // https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
 auto WINAPI wWinMain(HINSTANCE /* hinstance */,
