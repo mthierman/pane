772663699c8452f79e53156bab8914941399362b
mthierman <mthierman@gmail.com>
2025-04-26 13:44:04 -0600

    Update windows example

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -5,11 +5,12 @@ auto WINAPI wWinMain(HINSTANCE /* hinstance */,
                      HINSTANCE /* hprevinstance */,
                      PWSTR /* pcmdline */,
                      int /* ncmdshow */) -> int {
-    // auto window { pane::window() };
+    auto webview { pane::webview({}, { .home_page = u8"https://www.google.com/" }) };
 
+    // auto window { pane::window() };
     // window.~window();
 
-    auto win { std::make_unique<pane::window>() };
+    // auto win { std::make_unique<pane::window>() };
 
     return pane::system::message_loop();
 }
