4eced52cb9600d9f799dd176aabe9683fbc60bf5
mthierman <mthierman@gmail.com>
2025-04-26 19:00:04 -0600

    Cleanup example

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -1,38 +1,5 @@
 #include <pane/pane.hxx>
 
-auto make_window(pane::window_manager& window_manager) -> pane::window {
-    return pane::window({ u8"pane", pane::color { 255, 0, 0, 255 }, true, false },
-                        [&](pane::window::procedure procedure) -> LRESULT {
-        switch (procedure.msg.msg) {
-            case WM_CREATE: {
-                window_manager.insert(procedure.msg.hwnd);
-            } break;
-            case WM_DESTROY: {
-                window_manager.erase(procedure.msg.hwnd);
-            } break;
-        }
-
-        return procedure.msg.default_procedure();
-    });
-}
-
-auto make_webview(pane::window_manager& window_manager) -> pane::webview {
-    return pane::webview({ .background_color = pane::color { 16, 18, 24, 255 } },
-                         { .home_page = u8"https://www.google.com/" },
-                         [&](pane::webview::procedure procedure) -> LRESULT {
-        switch (procedure.msg.msg) {
-            case WM_CREATE: {
-                window_manager.insert(procedure.msg.hwnd);
-            } break;
-            case WM_DESTROY: {
-                window_manager.erase(procedure.msg.hwnd);
-            } break;
-        }
-
-        return procedure.msg.default_procedure();
-    });
-}
-
 // https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
 auto WINAPI wWinMain(HINSTANCE /* hinstance */,
                      HINSTANCE /* hprevinstance */,
@@ -40,8 +7,20 @@ auto WINAPI wWinMain(HINSTANCE /* hinstance */,
                      int /* ncmdshow */) -> int {
     auto window_manager { pane::window_manager() };
 
-    // auto window { make_window(window_manager) };
-    auto webview { make_webview(window_manager) };
+    auto webview { pane::webview({ .background_color = pane::color { 16, 18, 24, 255 } },
+                                 { .home_page = u8"https://www.google.com/" },
+                                 [&](pane::webview::procedure procedure) -> LRESULT {
+        switch (procedure.msg.msg) {
+            case WM_CREATE: {
+                window_manager.insert(procedure.msg.hwnd);
+            } break;
+            case WM_DESTROY: {
+                window_manager.erase(procedure.msg.hwnd);
+            } break;
+        }
+
+        return procedure.msg.default_procedure();
+    }) };
 
     return pane::system::message_loop();
 }
