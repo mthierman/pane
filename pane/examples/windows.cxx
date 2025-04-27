01058c9bd05c96ebfa72fb7aba3b2bce19a8b977
mthierman <mthierman@gmail.com>
2025-04-26 19:05:36 -0600

    Window example

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -7,9 +7,8 @@ auto WINAPI wWinMain(HINSTANCE /* hinstance */,
                      int /* ncmdshow */) -> int {
     auto window_manager { pane::window_manager() };
 
-    auto webview { pane::webview({ .background_color = pane::color { 16, 18, 24, 255 } },
-                                 { .home_page = u8"https://www.google.com/" },
-                                 [&](pane::webview::procedure procedure) -> LRESULT {
+    auto window { pane::window({ .background_color = pane::color { 16, 18, 24, 255 } },
+                               [&](pane::window::procedure procedure) -> LRESULT {
         switch (procedure.msg.msg) {
             case WM_CREATE: {
                 window_manager.insert(procedure.msg.hwnd);
@@ -21,6 +20,21 @@ auto WINAPI wWinMain(HINSTANCE /* hinstance */,
 
         return procedure.msg.default_procedure();
     }) };
+
+    // auto webview { pane::webview({ .background_color = pane::color { 16, 18, 24, 255 } },
+    //                              { .home_page = u8"https://www.google.com/" },
+    //                              [&](pane::webview::procedure procedure) -> LRESULT {
+    //     switch (procedure.msg.msg) {
+    //         case WM_CREATE: {
+    //             window_manager.insert(procedure.msg.hwnd);
+    //         } break;
+    //         case WM_DESTROY: {
+    //             window_manager.erase(procedure.msg.hwnd);
+    //         } break;
+    //     }
+
+    //     return procedure.msg.default_procedure();
+    // }) };
 
     return pane::system::message_loop();
 }
