bf5eefa5190dbb65996977552a3bc29bca5695bf
mthierman <mthierman@gmail.com>
2025-04-28 15:56:33 -0600

    Fix example

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -8,7 +8,7 @@ auto wWinMain(HINSTANCE /* hinstance */,
     auto window_manager { pane::window_manager() };
 
     auto window { pane::window(
-        { u8"test", pane::color { 0, 255, 0, 255 }, true, true, nullptr },
+        { u8"test", pane::color { 0, 255, 0, 255 }, true, nullptr },
         [&](pane::window* window, pane::window_message window_message) -> LRESULT {
         switch (window_message.msg) {
             case WM_CREATE: {
