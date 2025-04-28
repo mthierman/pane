af9fbb9c81f430de5ef2e1107317afb4219e5fbe
mthierman <mthierman@gmail.com>
2025-04-27 23:20:38 -0600

    Remove WINAPI

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -1,10 +1,10 @@
 #include <pane/pane.hxx>
 
 // https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
-auto WINAPI wWinMain(HINSTANCE /* hinstance */,
-                     HINSTANCE /* hprevinstance */,
-                     PWSTR /* pcmdline */,
-                     int /* ncmdshow */) -> int {
+auto wWinMain(HINSTANCE /* hinstance */,
+              HINSTANCE /* hprevinstance */,
+              PWSTR /* pcmdline */,
+              int /* ncmdshow */) -> int {
     auto window_manager { pane::window_manager() };
 
     auto window { pane::window({ .background_color = pane::color { 16, 18, 24, 255 } },
