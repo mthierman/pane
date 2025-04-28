bbc04b3d8503030abe67f8016b9656d2abb2bc56
mthierman <mthierman@gmail.com>
2025-04-27 23:27:56 -0600

    &window_manager

--- pane/examples/windows.cxx
+++ pane/examples/windows.cxx
@@ -7,8 +7,9 @@ auto wWinMain(HINSTANCE /* hinstance */,
               int /* ncmdshow */) -> int {
     auto window_manager { pane::window_manager() };
 
-    auto window { pane::window({ .background_color = pane::color { 16, 18, 24, 255 } },
-                               [&](pane::window* window, pane::window_message msg) -> LRESULT {
+    auto window { pane::window(
+        { .background_color = pane::color { 16, 18, 24, 255 } },
+        [&window_manager](pane::window* window, pane::window_message msg) -> LRESULT {
         switch (msg.msg) {
             case WM_CREATE: {
                 window_manager.insert(window->window_handle);
@@ -21,15 +22,16 @@ auto wWinMain(HINSTANCE /* hinstance */,
         return msg.default_procedure();
     }) };
 
-    auto webview { pane::webview({ .background_color = pane::color { 16, 18, 24, 255 } },
-                                 { .home_page = u8"https://www.google.com/" },
-                                 [&](pane::webview* window, pane::window_message msg) -> LRESULT {
+    auto webview { pane::webview(
+        { .background_color = pane::color { 16, 18, 24, 255 } },
+        { .home_page = u8"https://www.google.com/" },
+        [&window_manager](pane::webview* webview, pane::window_message msg) -> LRESULT {
         switch (msg.msg) {
             case WM_CREATE: {
-                window_manager.insert(window->window_handle);
+                window_manager.insert(webview->window_handle);
             } break;
             case WM_DESTROY: {
-                window_manager.erase(window->window_handle);
+                window_manager.erase(webview->window_handle);
             } break;
         }
 
