9f656e95e37d1f144bd5275c9b2efc619e7e4f2f
mthierman <mthierman@gmail.com>
2025-04-29 15:35:46 -0600

    Add windows_ptr example

---
+++ pane/examples/windows_ptr.cxx
@@ -0,0 +1,46 @@
+#include <pane/pane.hxx>
+
+// https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
+auto wWinMain(HINSTANCE /* hinstance */,
+              HINSTANCE /* hprevinstance */,
+              PWSTR /* pcmdline */,
+              int /* ncmdshow */) -> int {
+    auto window_manager { pane::window_manager<pane::window>() };
+
+    auto window { std::make_unique<pane::window>(
+        pane::window_config { u8"window", pane::color { 0, 0, 0, 255 }, true, nullptr },
+        [&](pane::window* window, pane::window_message window_message) -> LRESULT {
+        switch (window_message.msg) {
+            case WM_CREATE: {
+                window_manager.insert(window);
+            } break;
+            case WM_DESTROY: {
+                window_manager.erase(window);
+            } break;
+        }
+
+        return window_message.default_procedure();
+    }) };
+
+    window.reset();
+
+    // auto webview_manager { pane::window_manager<pane::webview>() };
+
+    // auto webview { pane::webview(
+    //     { u8"webview", pane::color { 0, 0, 0, 255 }, true, nullptr },
+    //     { .home_page = u8"https://www.google.com/" },
+    //     [&](pane::webview* webview, pane::window_message window_message) -> LRESULT {
+    //     switch (window_message.msg) {
+    //         case WM_CREATE: {
+    //             webview_manager.insert(webview);
+    //         } break;
+    //         case WM_DESTROY: {
+    //             webview_manager.erase(webview);
+    //         } break;
+    //     }
+
+    //     return window_message.default_procedure();
+    // }) };
+
+    return pane::system::message_loop();
+}
