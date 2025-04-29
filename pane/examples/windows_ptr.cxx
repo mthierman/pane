2aa9f75e1bef38d51ad34c1c0535efc6a00b4103
mthierman <mthierman@gmail.com>
2025-04-29 15:52:13 -0600

    Update examples

--- pane/examples/windows_ptr.cxx
+++ pane/examples/windows_ptr.cxx
@@ -22,23 +22,23 @@ auto wWinMain(HINSTANCE /* hinstance */,
         return window_message.default_procedure();
     }) };
 
-    window.reset();
+    // window.reset();
 
-    auto webview { pane::webview(
-        { u8"webview", pane::color { 0, 0, 0, 255 }, true, nullptr },
-        { .home_page = u8"https://www.google.com/" },
-        [&](pane::webview* webview, pane::window_message window_message) -> LRESULT {
-        switch (window_message.msg) {
-            case WM_CREATE: {
-                window_manager.insert(webview->window_handle);
-            } break;
-            case WM_DESTROY: {
-                window_manager.erase(webview->window_handle);
-            } break;
-        }
+    // auto webview { pane::webview(
+    //     { u8"webview", pane::color { 0, 0, 0, 255 }, true, nullptr },
+    //     { .home_page = u8"https://www.google.com/" },
+    //     [&](pane::webview* webview, pane::window_message window_message) -> LRESULT {
+    //     switch (window_message.msg) {
+    //         case WM_CREATE: {
+    //             window_manager.insert(webview->window_handle);
+    //         } break;
+    //         case WM_DESTROY: {
+    //             window_manager.erase(webview->window_handle);
+    //         } break;
+    //     }
 
-        return window_message.default_procedure();
-    }) };
+    //     return window_message.default_procedure();
+    // }) };
 
     return pane::system::message_loop();
 }
