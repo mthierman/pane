fe18a0b32e0b25d5caff2e441055330bd5d45c79
mthierman <mthierman@gmail.com>
2025-04-29 23:00:31 -0600

    Simplify webview wndproc

--- pane/examples/advanced/webview/webview.cxx
+++ pane/examples/advanced/webview/webview.cxx
@@ -4,18 +4,13 @@ auto wWinMain(HINSTANCE /* hinstance */,
               HINSTANCE /* hprevinstance */,
               PWSTR /* pcmdline */,
               int /* ncmdshow */) -> int {
-    auto window_manager { pane::window_manager() };
-
     auto webview { pane::webview(
         { u8"webview", pane::color { 0, 0, 0, 255 }, true, nullptr },
-        { .home_page = u8"https://www.google.com/" },
+        { .home_page = u8"about:blank" },
         [&](pane::webview* webview, pane::window_message window_message) -> LRESULT {
         switch (window_message.event) {
             case WM_CREATE: {
-                window_manager.insert(webview->window_handle);
-            } break;
-            case WM_DESTROY: {
-                window_manager.erase(webview->window_handle);
+                pane::debug("WM_CREATE");
             } break;
         }
 
