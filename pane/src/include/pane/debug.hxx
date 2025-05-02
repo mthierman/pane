612076b9268b20606f3ad82ffbc3a88d157ebf66
mthierman <mthierman@gmail.com>
2025-05-02 14:11:39 -0600

    Fix debug_message templates

--- pane/src/include/pane/debug.hxx
+++ pane/src/include/pane/debug.hxx
@@ -12,33 +12,34 @@ auto debug(const std::u16string& string) -> void;
 
 template <typename... Args>
 auto debug(std::format_string<Args...> format_string, Args&&... args) -> void {
-    auto fmt { pane::to_utf16(std::vformat(format_string.get(), std::make_format_args(args...))) };
+    const auto u16string { pane::to_utf16(
+        std::vformat(format_string.get(), std::make_format_args(args...))) };
 
-    OutputDebugStringW(reinterpret_cast<const wchar_t*>(fmt.data()));
+    OutputDebugStringW(reinterpret_cast<const wchar_t*>(u16string.data()));
     OutputDebugStringW(L"\n");
 }
 
 template <typename... Args>
 auto debug(std::wformat_string<Args...> format_string, Args&&... args) -> void {
-    OutputDebugStringW(std::vformat(format_string.get(), std::make_wformat_args(args...)).c_str());
+    OutputDebugStringW(std::vformat(format_string.get(), std::make_wformat_args(args...)).data());
     OutputDebugStringW(L"\n");
 }
 
 template <typename... Args>
 auto debug_message(std::format_string<Args...> format_string, Args&&... args) -> void {
-    if (auto converted_format_string {
-            pane::to_utf16(std::vformat(format_string.get(), std::make_format_args(args...))) }) {
-        MessageBoxW(nullptr,
-                    reinterpet_cast<const wchar_t*>(converted_format_string.data()),
-                    L"Info",
-                    MB_OK | MB_ICONASTERISK);
-    }
+    const auto u16string { pane::to_utf16(
+        std::vformat(format_string.get(), std::make_format_args(args...))) };
+
+    MessageBoxW(nullptr,
+                reinterpet_cast<const wchar_t*>(u16string.data()),
+                L"Info",
+                MB_OK | MB_ICONASTERISK);
 }
 
 template <typename... Args>
 auto debug_message(std::wformat_string<Args...> format_string, Args&&... args) -> void {
     MessageBoxW(nullptr,
-                std::vformat(format_string.get(), std::make_wformat_args(args...)).c_str(),
+                std::vformat(format_string.get(), std::make_wformat_args(args...)).data(),
                 L"Info",
                 MB_OK | MB_ICONASTERISK);
 }
