8b794a3f88f89719bc958ab956b15726da2ebd04
mthierman <mthierman@gmail.com>
2025-05-02 14:19:51 -0600

    Add window example

---
+++ pane/examples/window/main.cxx
@@ -0,0 +1,27 @@
+#include <pane/pane.hxx>
+
+// https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
+auto wWinMain(HINSTANCE /* hinstance */,
+              HINSTANCE /* hprevinstance */,
+              PWSTR /* pcmdline */,
+              int /* ncmdshow */) -> int {
+    auto window_manager { pane::window_manager() };
+
+    auto window_background { pane::color { 0, 0, 0, 255 } };
+    auto window { pane::window(
+        { u8"window", window_background, true, nullptr },
+        [&](pane::window* window, pane::window_message window_message) -> LRESULT {
+        switch (window_message.event) {
+            case WM_CREATE: {
+                window_manager.insert(window->window_handle);
+            } break;
+            case WM_DESTROY: {
+                window_manager.erase(window->window_handle);
+            } break;
+        }
+
+        return window_message.default_procedure();
+    }) };
+
+    return pane::system::message_loop();
+}
