305b164217ef0726fb3bf05d76fd864079b1b437
mthierman <mthierman@gmail.com>
2025-05-06 16:02:45 -0600

    Add docs

---
+++ docs/main.cxx
@@ -0,0 +1,28 @@
+#include <pane/pane.hxx>
+
+// https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
+auto wWinMain(HINSTANCE /* hinstance */,
+              HINSTANCE /* hprevinstance */,
+              PWSTR /* pcmdline */,
+              int /* ncmdshow */) -> int {
+    auto window_manager { pane::window_manager() };
+
+    auto url { pane::webview(
+        { u8"url", pane::color { 0, 0, 0, 0 }, pane::color { 255, 255, 255, 0 }, true, nullptr },
+        { .home_page = u8"https://learn.microsoft.com/en-us/microsoft-edge/webview2/concepts/"
+                       u8"working-with-local-content?tabs=dotnetcsharp" },
+        [&](pane::webview* webview, pane::window_message window_message) -> LRESULT {
+        switch (window_message.event) {
+            case WM_CREATE: {
+                window_manager.insert(webview->window_handle);
+            } break;
+            case WM_DESTROY: {
+                window_manager.erase(webview->window_handle);
+            } break;
+        }
+
+        return webview->default_procedure(window_message);
+    }) };
+
+    return pane::system::message_loop();
+}
