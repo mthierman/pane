4f6c17ea038d50645ab9666b64c7b901e8c70956
mthierman <mthierman@gmail.com>
2025-05-05 21:49:35 -0600

    Use .internal

--- pane/examples/webview/main.cxx
+++ pane/examples/webview/main.cxx
@@ -32,11 +32,11 @@ auto wWinMain(HINSTANCE /* hinstance */,
         // webview->navigate_to_string(u8R"(<html><body>raw</body></html>)");
 
         webview->core->SetVirtualHostNameToFolderMapping(
-            L"webview.localhost",
+            L"webview.internal",
             L"D:/mthierman/pane/pane/data",
             COREWEBVIEW2_HOST_RESOURCE_ACCESS_KIND::COREWEBVIEW2_HOST_RESOURCE_ACCESS_KIND_ALLOW);
 
-        webview->navigate(u8"http://webview.localhost/index.html");
+        webview->navigate(u8"http://webview.internal/index.html");
         // webview->navigate(u8"D:/mthierman/pane/pane/data/index.html");
 
         pane::debug("OK!");
