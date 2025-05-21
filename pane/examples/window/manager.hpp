f58ec0bce2b6bb9de9544654355ec208faa98570
mthierman <mthierman@gmail.com>
2025-05-20 21:41:43 -0600

    Migrate methods and fix build

--- pane/examples/window/manager.hpp
+++ pane/examples/window/manager.hpp
@@ -2,10 +2,22 @@
 #include <pane/pane.hxx>
 
 namespace pane::example {
-struct window_manager {
+struct window_manager final {
     using Self = window_manager;
 
     window_manager(pane::window_config&& window_config = {});
     ~window_manager() = default;
+
+    auto insert(this Self& self, const pane::window_handle& window_handle) -> void;
+    auto erase(this Self& self, const pane::window_handle& window_handle) -> void;
+    auto clear(this Self& self) -> void;
+    auto size(this const Self& self) -> uint64_t;
+    auto contains(this const Self& self, HWND hwnd) -> bool;
+    auto empty(this const Self& self) -> bool;
+    auto first(this const Self& self) -> HWND;
+    auto last(this const Self& self) -> HWND;
+
+private:
+    std::set<HWND> set;
 };
 } // namespace pane::example
