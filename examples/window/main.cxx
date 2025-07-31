f9e1fc23a991a93005ac92020b17f6789cc1c2a9
mthierman <mthierman@gmail.com>
2025-07-31 15:11:28 -0600

    Use switch in app window

--- examples/window/main.cxx
+++ examples/window/main.cxx
@@ -13,57 +13,59 @@ auto wWinMain(HINSTANCE /* hinstance */,
 
     pane::message_window app { [&](const pane::window_message& window_message,
                                    pane::message_window& app) -> LRESULT {
-        if (window_message.msg == +message::NEW_WINDOW) {
-            window_manager.add(
-                pane::window_config { u8"windows",
-                                      pane::color { 0, 0, 0, 255 },
-                                      pane::color { 255, 255, 255, 255 },
-                                      true,
-                                      nullptr },
-                [&](const pane::window_message& window_message, pane::window& window) -> LRESULT {
-                switch (window_message.msg) {
-                    case WM_CREATE: {
-                    } break;
+        switch (window_message.msg) {
+            case +message::NEW_WINDOW: {
+                window_manager.add(pane::window_config { u8"windows",
+                                                         pane::color { 0, 0, 0, 255 },
+                                                         pane::color { 255, 255, 255, 255 },
+                                                         true,
+                                                         nullptr },
+                                   [&](const pane::window_message& window_message,
+                                       pane::window& window) -> LRESULT {
+                    switch (window_message.msg) {
+                        case WM_CREATE: {
+                        } break;
 
-                    case WM_CLOSE: {
-                        pane::window_message { app.window_handle(),
-                                               +message::CLOSE_WINDOW,
-                                               0,
-                                               (LPARAM)window.window_handle() }
-                            .send();
-                    } break;
+                        case WM_CLOSE: {
+                            pane::window_message { app.window_handle(),
+                                                   +message::CLOSE_WINDOW,
+                                                   0,
+                                                   (LPARAM)window.window_handle() }
+                                .send();
+                        } break;
 
-                    case WM_KEYDOWN: {
-                        switch (window_message.wparam) {
-                            case 'N': {
-                                if (pane::input::is_key_down(VK_LCONTROL)
-                                    || pane::input::is_key_down(VK_RCONTROL)) {
-                                    pane::window_message {
-                                        app.window_handle(), +message::NEW_WINDOW, 0, 0
+                        case WM_KEYDOWN: {
+                            switch (window_message.wparam) {
+                                case 'N': {
+                                    if (pane::input::is_key_down(VK_LCONTROL)
+                                        || pane::input::is_key_down(VK_RCONTROL)) {
+                                        pane::window_message {
+                                            app.window_handle(), +message::NEW_WINDOW, 0, 0
+                                        }
+                                            .send();
                                     }
-                                        .send();
-                                }
-                            } break;
-                            case 'W': {
-                                if (pane::input::is_key_down(VK_LCONTROL)
-                                    || pane::input::is_key_down(VK_RCONTROL)) {
-                                    pane::window_message { app.window_handle(),
-                                                           +message::CLOSE_WINDOW,
-                                                           0,
-                                                           (LPARAM)window.window_handle() }
-                                        .send();
-                                }
-                            } break;
-                        }
-                    } break;
-                }
+                                } break;
+                                case 'W': {
+                                    if (pane::input::is_key_down(VK_LCONTROL)
+                                        || pane::input::is_key_down(VK_RCONTROL)) {
+                                        pane::window_message { app.window_handle(),
+                                                               +message::CLOSE_WINDOW,
+                                                               0,
+                                                               (LPARAM)window.window_handle() }
+                                            .send();
+                                    }
+                                } break;
+                            }
+                        } break;
+                    }
 
-                return window.default_procedure(window_message);
-            });
-        }
+                    return window.default_procedure(window_message);
+                });
+            } break;
 
-        if (window_message.msg == +message::CLOSE_WINDOW) {
-            window_manager.remove((HWND)window_message.lparam);
+            case +message::CLOSE_WINDOW: {
+                window_manager.remove((HWND)window_message.lparam);
+            } break;
         }
 
         return app.default_procedure(window_message);
