23953d1b14d7808e456d43bfb872619d27a3adc3
mthierman <mthierman@gmail.com>
2025-07-31 12:35:16 -0600

    Store windows in creation_order

--- examples/window/main.cxx
+++ examples/window/main.cxx
@@ -1,7 +1,54 @@
 #include <pane/pane.hxx>
+#include <map>
+#include <chrono>
+#include <memory>
 
 enum struct message : int {
-    NOTIFY = WM_APP,
+    NEW_WINDOW = WM_APP,
+};
+
+struct window_manager final {
+    using Self = window_manager;
+
+    auto add(this Self& self) -> void {
+        self.windows.emplace(
+            std::chrono::steady_clock::now(),
+            std::make_unique<pane::window>(
+                pane::window_config { u8"window",
+                                      pane::color { 0, 0, 0, 255 },
+                                      pane::color { 255, 255, 255, 255 },
+                                      true,
+                                      nullptr },
+                [&](const pane::window_message& window_message, pane::window& window) -> LRESULT {
+            switch (window_message.msg) {
+                case WM_CREATE: {
+                } break;
+
+                case WM_CLOSE: {
+                    // self.remove(window);
+                } break;
+
+                case WM_KEYDOWN: {
+                    switch (window_message.wparam) {
+                        case 'N': {
+                            if (pane::input::is_key_down(VK_LCONTROL)
+                                || pane::input::is_key_down(VK_RCONTROL)) {
+                                self.add();
+                            }
+                        } break;
+                    }
+                } break;
+            }
+
+            return window.default_procedure(window_message);
+        }));
+    }
+
+    // auto remove(this Self& self, const pane::window& window) -> void {
+    //     self.windows.erase(window.window_handle());
+    // }
+
+    std::map<std::chrono::steady_clock::time_point, std::unique_ptr<pane::window>> windows;
 };
 
 // https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
@@ -9,50 +56,18 @@ auto wWinMain(HINSTANCE /* hinstance */,
               HINSTANCE /* hprevinstance */,
               PWSTR /* pcmdline */,
               int /* ncmdshow */) -> int {
-    pane::window_manager window_manager;
+    window_manager window_manager;
+
     pane::message_window app { [&](const pane::window_message& window_message,
                                    pane::message_window& app) -> LRESULT {
-        if (window_message.msg == +message::NOTIFY) {
-            pane::debug("message::NOTIFY");
+        if (window_message.msg == +message::NEW_WINDOW) {
+            window_manager.add();
         }
 
         return app.default_procedure(window_message);
     } };
 
-    auto window { pane::window {
-        { u8"window",
-          pane::color { 0, 0, 0, 255 },
-          pane::color { 255, 255, 255, 255 },
-          true,
-          nullptr },
-        [&](const pane::window_message& window_message, pane::window& window) -> LRESULT {
-        switch (window_message.msg) {
-            case WM_CREATE: {
-                window_manager.insert(window.window_handle);
-
-                pane::window_message { app.window_handle(), +message::NOTIFY, 0, 0 }.send();
-            } break;
-
-            case WM_DESTROY: {
-                window_manager.erase(window.window_handle);
-            } break;
-
-            case WM_KEYDOWN: {
-                switch (window_message.wparam) {
-                    case 'N': {
-                        if (pane::input::is_key_down(VK_LCONTROL)
-                            || pane::input::is_key_down(VK_RCONTROL)) {
-                            pane::debug("Ctrl+N");
-                        } else {
-                            pane::debug("N");
-                        }
-                    } break;
-                }
-            } break;
-        }
-
-        return window.default_procedure(window_message);
-    } } };
+    pane::window_message { app.window_handle(), +message::NEW_WINDOW, 0, 0 }.send();
 
     return pane::system::message_loop();
 }
