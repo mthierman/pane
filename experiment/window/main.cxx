6e5c8c64535945f3cea02eb8b65df433db65e4f8
mthierman <mthierman@gmail.com>
2025-08-01 18:45:58 -0600

    Add experiments folder

---
+++ experiment/window/main.cxx
@@ -0,0 +1,43 @@
+#include <pane/pane.hxx>
+
+// https://learn.microsoft.com/en-us/windows/win32/learnwin32/winmain--the-application-entry-point
+auto wWinMain(HINSTANCE /* hinstance */,
+              HINSTANCE /* hprevinstance */,
+              PWSTR /* pcmdline */,
+              int /* ncmdshow */) -> int {
+    pane::window_manager<pane::window> window_manager {
+        { u8"window",
+          pane::color { 0, 0, 0, 255 },
+          pane::color { 255, 255, 255, 255 },
+          true,
+          nullptr },
+        [&](const pane::window_message& window_message, pane::window& window) -> LRESULT {
+        switch (window_message.msg) {
+            case WM_CLOSE: {
+                window_manager.destroy(window.window_handle());
+            } break;
+
+            case WM_KEYDOWN: {
+                switch (window_message.wparam) {
+                    case 'N': {
+                        if (pane::input::is_key_down(VK_LCONTROL)
+                            || pane::input::is_key_down(VK_RCONTROL)) {
+                            window_manager.create();
+                        }
+                    } break;
+                    case 'W': {
+                        if (pane::input::is_key_down(VK_LCONTROL)
+                            || pane::input::is_key_down(VK_RCONTROL)) {
+                            SendMessageW(window.window_handle(), WM_CLOSE, 0, 0);
+                        }
+                    } break;
+                }
+            } break;
+        }
+
+        return window.default_procedure(window_message);
+    }
+    };
+
+    return pane::system::message_loop();
+}
