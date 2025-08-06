3ad73d8536098b6c28f4110ba2628edd317a0da2
mthierman <mthierman@gmail.com>
2025-08-05 23:09:52 -0600

    Add new libs installer script

---
+++ scripts/libs.ps1
@@ -0,0 +1,15 @@
+Push-Location
+$packages = (Get-Item $PSScriptRoot).Parent.FullName | Join-Path -ChildPath "packages"
+if (-not (Test-Path $packages))
+{
+    New-Item -Path $packages -ItemType Directory | Out-Null
+}
+Set-Location $packages
+gh release -R ada-url/ada download -p 'singleheader.zip' --skip-existing | Out-Null
+7z x .\singleheader.zip -oada-url | Out-Null
+gh release -R stephenberry/glaze download -A zip --skip-existing | Out-Null
+7z x .\glaze*.zip -ostephenberry | Out-Null
+nuget install Microsoft.Web.WebView2 -DirectDownload -x -OutputDirectory nuget -Verbosity quiet | Out-Null
+nuget install Microsoft.Windows.ImplementationLibrary -DirectDownload -x -OutputDirectory nuget -Verbosity quiet | Out-Null
+nuget install Microsoft.Windows.CppWinRT -DirectDownload -x -OutputDirectory nuget -Verbosity quiet | Out-Null
+Pop-Location
