fb55962dfc0f16c25596e0e9cb88f66898c11712
mthierman <mthierman@gmail.com>
2025-08-11 12:20:39 -0600

    Test typing data

--- vite/index.ts
+++ vite/index.ts
@@ -1,9 +1,8 @@
 import "./styles/index.css";
-import type { WebViewMessageEvent } from "./webview";
 
-window.chrome.webview.addEventListener("message", (event: WebViewMessageEvent) => {
+type Data = { message: string };
+
+window.chrome.webview.addEventListener<Data>("message", (event) => {
     const data = event.data;
-    console.log(event);
-    console.log(event.data);
-    document.getElementById("root")!.innerHTML = event.data.message;
+    document.getElementById("root")!.innerHTML = data.message;
 });
