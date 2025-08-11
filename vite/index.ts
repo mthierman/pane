7e74ae7f7ce7f43f32dc16368c05a6e2d31306b0
mthierman <mthierman@gmail.com>
2025-08-11 15:14:27 -0600

    Rename WebViewMessage

--- vite/index.ts
+++ vite/index.ts
@@ -3,11 +3,11 @@ import "./styles/index.css";
 type DataOne = { type: "init"; payload: { name: string; age: number } };
 type DataTwo = { type: "test"; payload: { test_msg: string; test_number: number } };
 
-type Data = DataOne | DataTwo;
+type WebViewMessage = DataOne | DataTwo;
 
 const root = document.getElementById("root") as HTMLDivElement;
 
-window.chrome.webview.addEventListener<Data>("message", (event) => {
+window.chrome.webview.addEventListener<WebViewMessage>("message", (event) => {
     const data = event.data;
     switch (data.type) {
         case "init":
