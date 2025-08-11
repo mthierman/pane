e35b96fb9005bdcf06302d59949b12f8f7f55e71
mthierman <mthierman@gmail.com>
2025-08-11 00:59:50 -0600

    Print JSON message to div

--- vite/index.ts
+++ vite/index.ts
@@ -3,4 +3,6 @@ import type { WebViewMessageEvent } from "./webview";
 
 window.chrome.webview.addEventListener("message", (event: WebViewMessageEvent) => {
     console.log(event);
+    console.log(event.data);
+    document.getElementById("root")!.innerHTML = event.data.message;
 });
