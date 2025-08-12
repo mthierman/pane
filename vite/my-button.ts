c4acb395e1ee3f8ec1754c71beed0506a09e0283
mthierman <mthierman@gmail.com>
2025-08-11 18:05:17 -0600

    Web Components (no shadow DOM)

--- vite/my-button.ts
+++ vite/my-button.ts
@@ -1,13 +1,22 @@
+// class MyButton extends HTMLElement {
+//     constructor() {
+//         super();
+//         const shadow = this.attachShadow({ mode: "open" });
+//         const button = document.createElement("button");
+//         button.textContent = "Click me";
+//         button.onclick = () => {
+//             console.log("Button clicked!");
+//         };
+//         shadow.appendChild(button);
+//     }
+// }
+
+// customElements.define("my-button", MyButton);
+
 class MyButton extends HTMLElement {
-    constructor() {
-        super();
-        const shadow = this.attachShadow({ mode: "open" });
-        const button = document.createElement("button");
-        button.textContent = "Click me";
-        button.onclick = () => {
-            console.log("Button clicked!");
-        };
-        shadow.appendChild(button);
+    connectedCallback() {
+        this.innerHTML = `<button>Click me</button>`;
+        this.querySelector("button")!.onclick = () => console.log("Button clicked!");
     }
 }
 
