9c33da77b2c13ba8f59ac362b2ee6615d64c4b38
mthierman <mthierman@gmail.com>
2025-08-12 21:59:05 -0600

    static define for web components

--- vite/button.ts
+++ vite/button.ts
@@ -1,6 +1,10 @@
 export class Button extends HTMLElement {
     button = document.createElement("button");
 
+    static define(tag = "pane-button") {
+        customElements.define(tag, this);
+    }
+
     connectedCallback() {
         this.button.textContent = "Click me";
         this.button.onclick = () => console.log("Button clicked!");
@@ -8,4 +12,4 @@ export class Button extends HTMLElement {
     }
 }
 
-customElements.define("pane-button", Button);
+Button.define();
