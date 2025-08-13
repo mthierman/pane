81974e7f5d5092860ddd3f48a7619c448e66c22b
mthierman <mthierman@gmail.com>
2025-08-12 20:43:18 -0600

    Add proper specialization for u8string to JSON

--- include/pane/glaze.hxx
+++ include/pane/glaze.hxx
@@ -2,6 +2,17 @@
 #include <string>
 #include <glaze/glaze.hpp>
 
-template <> struct glz::meta<std::u8string> {
-    static constexpr auto value = [](auto&& self) { return std::string(self.begin(), self.end()); };
+// template <> struct glz::meta<std::u8string> {
+//     static constexpr auto value = [](auto&& self) { return std::string(self.begin(), self.end());
+//     };
+// };
+
+namespace glz {
+template <> struct to<JSON, std::u8string> {
+    template <auto Opts>
+    static void
+    op(const std::u8string& value, is_context auto&& ctx, auto&& b, auto&& ix) noexcept {
+        serialize<JSON>::op<Opts>(std::string { value.begin(), value.end() }, ctx, b, ix);
+    }
 };
+} // namespace glz
