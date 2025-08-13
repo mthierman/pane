edd523282b473bf14faac46c21626b9a962deda2
mthierman <mthierman@gmail.com>
2025-08-12 20:55:50 -0600

    Fix from specialization

--- include/pane/glaze.hxx
+++ include/pane/glaze.hxx
@@ -2,16 +2,13 @@
 #include <string>
 #include <glaze/glaze.hpp>
 
-// template <> struct glz::meta<std::u8string> {
-//     static constexpr auto value = [](auto&& self) { return std::string(self.begin(), self.end());
-//     };
-// };
-
 namespace glz {
 template <> struct from<JSON, std::u8string> {
     template <auto Opts>
-    static void op(const std::u8string& value, is_context auto&& ctx, auto&& it, auto&& end) {
-        parse<JSON>::op<Opts>(std::u8string { value.begin(), value.end() }, ctx, it, end);
+    static void op(std::u8string& value, is_context auto&& ctx, auto&& it, auto&& end) {
+        std::string buffer;
+        parse<JSON>::op<Opts>(buffer, ctx, it, end);
+        value.assign(buffer.begin(), buffer.end());
     }
 };
 
