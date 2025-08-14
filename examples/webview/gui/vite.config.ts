1c9106e4c7658590d90e4cedad96691cf381cfde
mthierman <mthierman@gmail.com>
2025-08-14 08:25:07 -0600

    Remove oxlint

--- examples/webview/gui/vite.config.ts
+++ examples/webview/gui/vite.config.ts
@@ -1,11 +1,9 @@
 import { type CommonServerOptions, type UserConfig, defineConfig } from "vite";
-import oxlintPlugin from "vite-plugin-oxlint";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
     const userConfig: UserConfig = {
         base: "./",
-        plugins: [oxlintPlugin()],
     };
 
     const commonServerOptions: CommonServerOptions = {};
