a29da7123c8b9c75b6a7900f2eb55c44daa2e4ae
mthierman <mthierman@gmail.com>
2025-08-14 20:04:59 -0600

    Re-name event_payload

--- examples/webview/gui/src/webview_message.ts
+++ examples/webview/gui/src/webview_message.ts
@@ -1,8 +1,8 @@
-export type payload = {
+export type event_payload = {
     init: { name: string; age: number };
     test: { one: number; two: number };
 };
 
-export type webview_message<T = payload> = {
+export type webview_message<T = event_payload> = {
     [K in keyof T]: { type: K; payload: T[K] };
 }[keyof T];
