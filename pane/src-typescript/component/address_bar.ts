519700b0c258b9249d901b22b4fe08df7ee99f2d
mthierman <mthierman@gmail.com>
2025-08-16 17:58:37 -0600

    Use form for address bar

--- pane/src-typescript/component/address_bar.ts
+++ pane/src-typescript/component/address_bar.ts
@@ -13,10 +13,15 @@ export class AddressBar extends Component {
         }
     }
 
+    #form = document.createElement("form");
+    #input = document.createElement("input");
     #url = new URL("about:blank");
-    #input = document.createElement("input") as HTMLInputElement;
 
     get() {
+        return this.#form;
+    }
+
+    get input() {
         return this.#input;
     }
 
@@ -30,34 +35,58 @@ export class AddressBar extends Component {
     }
 
     connectedCallback() {
-        this.appendChild(this.#input);
+        this.appendChild(this.#form);
+        this.#form.appendChild(this.#input);
 
-        this.addEventListener("init", (event) => {
-            console.log("init");
-            this.url = event.detail.url;
-        });
+        this.#form.addEventListener("submit", (event) => {
+            event.preventDefault();
 
-        this.addEventListener("keydown", (event) => {
-            // console.log(`keydown ${event.key}`);
-            // if (event.key === "Enter") {
-            //     const test: WebViewMessageEventData<payload> = {
-            //         type: "init",
-            //         payload: {
-            //             url: this.url.href,
-            //         },
-            //     };
-            //     window.chrome.webview.postMessage(test);
-            // }
-            // this.dispatchEvent();
-        });
+            if (!/\.\w+$/.test(this.#input.value)) {
+                // Is search query
+            }
 
-        this.addEventListener("submit", () => {
             if (!this.#input.value.startsWith("http")) {
+                if (!/\.\w+$/.test(this.#input.value)) {
+                    this.#input.value = `${this.#input.value}.com`;
+                }
                 this.url = `https://${this.#input.value}`;
-                console.log(this.url);
             } else {
                 this.#input.value = this.url.toString();
             }
+
+            // this.url = this.#input.value;
         });
+
+        // this.#input.addEventListener("change", () => {
+        //     if (!this.#input.value.startsWith("http")) {
+        //         this.url = `https://${this.#input.value}`;
+        //         console.log(this.url);
+        //     } else {
+        //         this.#input.value = this.url.toString();
+        //     }
+        // });
+
+        // this.#input.addEventListener("keydown", (event) => {
+        //     console.log(`keydown ${event.key}`);
+        //     if (event.key === "Enter") {
+        //         const test: WebViewMessageEventData<payload> = {
+        //             type: "init",
+        //             payload: {
+        //                 url: this.url.href,
+        //             },
+        //         };
+        //         window.chrome.webview.postMessage(test);
+        //     }
+        //     this.dispatchEvent();
+        // });
+
+        // this.#input.addEventListener("change", () => {
+        //     if (!this.#input.value.startsWith("http")) {
+        //         this.url = `https://${this.#input.value}`;
+        //         console.log(this.url);
+        //     } else {
+        //         this.#input.value = this.url.toString();
+        //     }
+        // });
     }
 }
