71ba4c0ec1be2e92f106dc905f8f2fe89b11a29e
mthierman <mthierman@gmail.com>
2025-08-16 01:05:20 -0600

    Remove getBody()

--- examples/webview/gui/src/utility.ts
+++ examples/webview/gui/src/utility.ts
@@ -1,12 +1,3 @@
 export function getElementById<T = HTMLElement>(elementId: string) {
     return document.getElementById(elementId) as T | null;
 }
-
-export function getBody() {
-    // let body: HTMLBodyElement;
-    // body = document.body as HTMLBodyElement;
-    // let body = document.body as HTMLBodyElement;
-    // return body;
-
-    return document.body as HTMLBodyElement | null;
-}
