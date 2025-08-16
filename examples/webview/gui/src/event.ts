74a2535f091549e7245881e816e2051daf5efb6b
mthierman <mthierman@gmail.com>
2025-08-16 10:41:04 -0600

    Remove event module

--- examples/webview/gui/src/event.ts
+++
@@ -1,8 +0,0 @@
-export class Event {
-    // static new<T extends Record<string, any>>(type: keyof T, detail: T[keyof T]) {
-    //     return new CustomEvent(type as string, { detail });
-    // }
-    // static new<T>(event: WebViewMessageEvent<WebViewMessageEventData<T>>) {
-    //     return new CustomEvent(event.data.type as string, { detail: event.data.payload });
-    // }
-}
