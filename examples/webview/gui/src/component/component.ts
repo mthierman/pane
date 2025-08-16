70f46d213300ee6967804108b49c6330b0762dba
mthierman <mthierman@gmail.com>
2025-08-16 10:42:36 -0600

    Remove Utility module

--- examples/webview/gui/src/component/component.ts
+++ examples/webview/gui/src/component/component.ts
@@ -2,6 +2,10 @@ export const body = document.body as HTMLBodyElement;
 
 export const html = document.documentElement as HTMLHtmlElement;
 
+export function getElementById<T = HTMLElement>(elementId: string) {
+    return document.getElementById(elementId) as T | null;
+}
+
 export class Component extends HTMLElement {
     static define(name: string, constructor: CustomElementConstructor) {
         if (!customElements.get(name)) {
--- examples/webview/gui/src/utility.ts
+++
@@ -1,8 +0,0 @@
-export const Utility = {
-    html: document.documentElement as HTMLHtmlElement,
-    body: document.body as HTMLBodyElement,
-
-    getElementById<T = HTMLElement>(elementId: string) {
-        return document.getElementById(elementId) as T | null;
-    },
-};
