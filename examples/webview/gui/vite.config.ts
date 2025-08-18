01577c7fa63b4e3722282864d5ab9970dbc67e5a
mthierman <mthierman@gmail.com>
2025-08-18 15:33:44 -0600

    vite-plugin-singlefile

--- examples/webview/gui/vite.config.ts
+++ examples/webview/gui/vite.config.ts
@@ -1,9 +1,11 @@
 import { type CommonServerOptions, type UserConfig, defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
     const userConfig: UserConfig = {
         base: "./",
+        plugins: [viteSingleFile()],
     };
 
     const commonServerOptions: CommonServerOptions = {};
