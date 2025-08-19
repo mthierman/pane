527789e811180b9658140d4c1dad6e83f2335bf0
mthierman <mthierman@gmail.com>
2025-08-18 22:17:59 -0600

    Move everything to index.ts

--- pane/typescript/index.ts
+++ pane/typescript/index.ts
@@ -1,5 +1,44 @@
-import { Component } from "./component";
-import { Page } from "./page";
 import "./webview";
 
-export { Component, Page };
+declare global {
+    interface CustomElementRegistry {
+        get<K extends keyof HTMLElementTagNameMap>(
+            name: K,
+        ): (new () => HTMLElementTagNameMap[K]) | undefined;
+        define<K extends keyof HTMLElementTagNameMap>(
+            name: K,
+            constructor: new () => HTMLElementTagNameMap[K],
+            options?: ElementDefinitionOptions,
+        ): void;
+    }
+}
+
+export class Page {
+    static html = document.documentElement as HTMLHtmlElement;
+    static head = document.head as HTMLHeadElement;
+    static body = document.body as HTMLBodyElement;
+
+    static createElement<K extends keyof HTMLElementTagNameMap>(tagName: K, id?: string) {
+        const element = document.createElement(tagName);
+        id ? element.setAttribute("id", id) : element.removeAttribute("id");
+        return element;
+    }
+
+    static getElementById<T = HTMLElement>(elementId: string) {
+        return document.getElementById(elementId) as T | null;
+    }
+}
+
+export class Component extends HTMLElement {
+    attach<T extends Node>(parent?: T) {
+        return (parent ?? Page.body).appendChild(this);
+    }
+
+    dispatch<T = unknown>(event: WebViewMessageEvent<T>) {
+        super.dispatchEvent(
+            new CustomEvent(event.data.type, {
+                detail: event.data.payload,
+            }),
+        );
+    }
+}
