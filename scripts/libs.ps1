b320c055772e50fdd32c2d91570b3667010a64ab
mthierman <mthierman@gmail.com>
2025-09-14 19:32:04 -0600

    Rename glaze package folder after download

--- scripts/libs.ps1
+++ scripts/libs.ps1
@@ -9,6 +9,7 @@ gh release -R ada-url/ada download -p 'singleheader.zip' --skip-existing | Out-N
 7z x .\singleheader.zip -oada-url/ada | Out-Null
 gh release -R stephenberry/glaze download -A zip --skip-existing | Out-Null
 7z x .\glaze*.zip -ostephenberry | Out-Null
+Rename-Item .\stephenberry\glaze-* glaze -Force
 nuget install Microsoft.Web.WebView2 -DirectDownload -x -OutputDirectory nuget -Verbosity quiet | Out-Null
 nuget install Microsoft.Windows.ImplementationLibrary -DirectDownload -x -OutputDirectory nuget -Verbosity quiet | Out-Null
 nuget install Microsoft.Windows.CppWinRT -DirectDownload -x -OutputDirectory nuget -Verbosity quiet | Out-Null
