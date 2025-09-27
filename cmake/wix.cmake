e4c2cdfe38eb6c796bab76c155c51e51d60babd9
mthierman <mthierman@gmail.com>
2025-09-26 22:13:08 -0600

    Update wix

--- cmake/wix.cmake
+++ cmake/wix.cmake
@@ -1,19 +1,29 @@
-find_program(DOTNET_EXECUTABLE dotnet NO_CACHE)
+find_program(WIX_EXECUTABLE wix NO_CACHE)
 
-if(IS_EXECUTABLE "${DOTNET_EXECUTABLE}")
-    if(NOT TARGET dotnet)
-        add_executable(dotnet IMPORTED GLOBAL)
-        set_target_properties(
-            dotnet
-            PROPERTIES IMPORTED_LOCATION "${DOTNET_EXECUTABLE}"
-        )
-        add_custom_target(
-            wix
-            COMMAND dotnet tool restore
-            COMMAND
-                dotnet wix extension add
-                WixToolset.BootstrapperApplications.wixext
-            COMMAND dotnet wix extension add WixToolset.UI.wixext
-        )
-    endif()
+add_custom_target(
+    wix_install
+    COMMAND dotnet tool install --global wix
+)
+
+add_custom_target(
+    wix_add_extensions
+    COMMAND wix extension add WixToolset.BootstrapperApplications.wixext
+    COMMAND wix extension add WixToolset.UI.wixext
+)
+
+if(NOT TARGET wix)
+    add_executable(wix IMPORTED GLOBAL)
+    set_target_properties(wix PROPERTIES IMPORTED_LOCATION "${WIX_EXECUTABLE}")
+    add_dependencies(wix wix_install wix_add_extensions)
 endif()
+
+# if(IS_EXECUTABLE "${WIX_EXECUTABLE}")
+#     if(NOT TARGET wix)
+#         add_executable(wix IMPORTED GLOBAL)
+#         set_target_properties(
+#             wix
+#             PROPERTIES IMPORTED_LOCATION "${WIX_EXECUTABLE}"
+#         )
+#         add_dependencies(wix wix_install wix_add_extensions)
+#     endif()
+# endif()
