c529acab60f1c3dadc76435d4f5ae0ed8697f7dc
mthierman <mthierman@gmail.com>
2025-09-26 23:14:47 -0600

    Cleanup wix

--- cmake/wix.cmake
+++ cmake/wix.cmake
@@ -1,5 +1,3 @@
-find_program(WIX_EXECUTABLE wix NO_CACHE)
-
 add_custom_target(
     install_wix
     WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
@@ -8,18 +6,14 @@ add_custom_target(
     COMMAND wix extension add WixToolset.UI.wixext
 )
 
-if(NOT TARGET wix)
-    add_executable(wix IMPORTED GLOBAL)
-    set_target_properties(wix PROPERTIES IMPORTED_LOCATION "${WIX_EXECUTABLE}")
-endif()
+find_program(WIX_EXECUTABLE wix NO_CACHE)
 
-# if(IS_EXECUTABLE "${WIX_EXECUTABLE}")
-#     if(NOT TARGET wix)
-#         add_executable(wix IMPORTED GLOBAL)
-#         set_target_properties(
-#             wix
-#             PROPERTIES IMPORTED_LOCATION "${WIX_EXECUTABLE}"
-#         )
-#         add_dependencies(wix wix_install wix_add_extensions)
-#     endif()
-# endif()
+if(IS_EXECUTABLE "${WIX_EXECUTABLE}")
+    if(NOT TARGET wix)
+        add_executable(wix IMPORTED GLOBAL)
+        set_target_properties(
+            wix
+            PROPERTIES IMPORTED_LOCATION "${WIX_EXECUTABLE}"
+        )
+    endif()
+endif()
