45b0a18c55cb1817ff9f41b7d6c879092aa339a7
mthierman <mthierman@gmail.com>
2025-09-28 00:35:22 -0600

    Remove wix module

--- cmake/wix.cmake
+++
@@ -1,8 +0,0 @@
-if(NOT TARGET wix)
-    find_program(WIX_EXECUTABLE wix NO_CACHE)
-
-    if(IS_EXECUTABLE "${WIX_EXECUTABLE}")
-        add_executable(wix IMPORTED GLOBAL)
-        set_target_properties(wix PROPERTIES IMPORTED_LOCATION "${WIX_EXECUTABLE}")
-    endif()
-endif()
