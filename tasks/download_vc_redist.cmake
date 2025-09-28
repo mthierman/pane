c572e17ed7c9824e5c2371865f9bb4e4f459d8d0
mthierman <mthierman@gmail.com>
2025-09-27 21:30:42 -0600

    Fix out_dir

--- tasks/download_vc_redist.cmake
+++ tasks/download_vc_redist.cmake
@@ -1,6 +1,4 @@
 file(
-    DOWNLOAD
-        https://aka.ms/vs/17/release/vc_redist.x64.exe
-        ${CMAKE_BINARY_DIR}/vc_redist.x64.exe
+    DOWNLOAD https://aka.ms/vs/17/release/vc_redist.x64.exe vc_redist.x64.exe
     SHOW_PROGRESS
 )
