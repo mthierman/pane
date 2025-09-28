fdb4a20b1e7507a9bca7769112c1336c5c54b3e9
mthierman <mthierman@gmail.com>
2025-09-27 21:02:21 -0600

    Guard whole wix target

--- cmake/wix.cmake
+++ cmake/wix.cmake
@@ -6,10 +6,10 @@ add_custom_target(
     COMMAND wix extension add WixToolset.UI.wixext
 )
 
-find_program(WIX_EXECUTABLE wix NO_CACHE)
+if(NOT TARGET wix)
+    find_program(WIX_EXECUTABLE wix NO_CACHE)
 
-if(IS_EXECUTABLE "${WIX_EXECUTABLE}")
-    if(NOT TARGET wix)
+    if(IS_EXECUTABLE "${WIX_EXECUTABLE}")
         add_executable(wix IMPORTED GLOBAL)
         set_target_properties(
             wix
