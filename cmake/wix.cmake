3ab9f3d9786879d6724272aebe26bae0bc748231
mthierman <mthierman@gmail.com>
2025-09-27 21:15:26 -0600

    Remove wix example

--- cmake/wix.cmake
+++ cmake/wix.cmake
@@ -1,11 +1,3 @@
-add_custom_target(
-    install_wix
-    WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-    COMMAND dotnet tool install --global wix
-    COMMAND wix extension add WixToolset.BootstrapperApplications.wixext
-    COMMAND wix extension add WixToolset.UI.wixext
-)
-
 if(NOT TARGET wix)
     find_program(WIX_EXECUTABLE wix NO_CACHE)
 
