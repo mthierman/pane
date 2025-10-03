c0ff1122b919056bc521ce1b877ff90eb0a1614f
mthierman <mthierman@gmail.com>
2025-10-02 22:20:27 -0600

    Cleanup unused char8_t* formatter

--- pane/include/pane/text.hpp
+++ pane/include/pane/text.hpp
@@ -46,12 +46,6 @@ template <> struct formatter<std::u8string_view> : formatter<string_view> {
     }
 };
 
-// template <> struct formatter<const char8_t*> : formatter<std::u8string_view> {
-//     auto format(const char8_t* string, format_context& context) const noexcept {
-//         return formatter<std::u8string_view>::format(std::u8string_view(string), context);
-//     }
-// };
-
 template <> struct formatter<std::u16string, wchar_t> : formatter<wstring_view, wchar_t> {
     auto format(const std::u16string& string, wformat_context& context) const noexcept {
         return formatter<wstring_view, wchar_t>::format(
