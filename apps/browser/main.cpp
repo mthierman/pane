71f3b5586e142373253a1b31827227c503aa470a
mthierman <mthierman@gmail.com>
2025-10-10 10:28:31 -0600

    Add browser install rule

--- apps/browser/main.cpp
+++ apps/browser/main.cpp
@@ -10,13 +10,13 @@ struct webview : pane::webview<webview> {
         auto args { argv.u8string() };
         std::optional<std::u8string> home_page { std::nullopt };
 
-        if (args.size() == 2) {
-            if (args.at(1).starts_with(u8"https://")) {
-                home_page = args.at(1);
-            } else {
-                home_page = args.at(1).insert(0, u8"https://");
-            }
-        }
+        // if (args.size() == 2) {
+        //     if (args.at(1).starts_with(u8"https://")) {
+        //         home_page = args.at(1);
+        //     } else {
+        //         home_page = args.at(1).insert(0, u8"https://");
+        //     }
+        // }
 
         std::filesystem::path browser_data;
         auto local_app_data { pane::filesystem::known_folder() };
@@ -30,7 +30,7 @@ struct webview : pane::webview<webview> {
                                        pane::color { 0, 0, 0, 255 },
                                        true,
                                        nullptr },
-                 pane::webview_config { home_page.value_or(u8"https://www.google.com/"),
+                 pane::webview_config { home_page.value_or(u8"http://localhost:4321/"),
                                         std::nullopt,
                                         u8"",
                                         browser_data } };
