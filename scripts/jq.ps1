4609b1090eecc52cda89ce1552ec37ebcc6fa00b
mthierman <mthierman@gmail.com>
2025-10-11 23:35:23 -0600

    Save jq output to build folder

--- scripts/jq.ps1
+++ scripts/jq.ps1
@@ -1,7 +1,9 @@
+# https://til.simonwillison.net/jq/git-log-json
+
 git log -5 --pretty=format:'%h%x00%an%x00%aI%x00%s%x00' | `
     jq -R -s '[split("\n")[:-1] | map(split("\u0000")) | .[] | {
     "commit": .[0],
     "author": .[1],
     "date": .[2],
     "message": .[3]
-  }]'
+  }]' | Out-File -Encoding utf8 "build/commits.json"
