637598b1c74072043da1feb393ab4631b595fdf2
mthierman <mthierman@gmail.com>
2025-10-11 23:33:57 -0600

    Add jq script

---
+++ scripts/jq.ps1
@@ -0,0 +1,7 @@
+git log -5 --pretty=format:'%h%x00%an%x00%aI%x00%s%x00' | `
+    jq -R -s '[split("\n")[:-1] | map(split("\u0000")) | .[] | {
+    "commit": .[0],
+    "author": .[1],
+    "date": .[2],
+    "message": .[3]
+  }]'
