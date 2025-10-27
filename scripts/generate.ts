7c41629dcd6f61652e445b3e537b451534c64332
mthierman <mthierman@gmail.com>
2025-10-27 13:21:31 -0600

    Use package_json

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,13 +1,14 @@
 import { Temporal } from "@js-temporal/polyfill";
 import { writeFile } from "node:fs/promises";
 import { dirname, join } from "node:path";
+import package_json from "../package.json" with { type: "json" };
 
 const config = {
     event_type: "pane-notify",
     client_payload: {
-        name: "pane",
-        version: "0.0.0",
-        description: "C++ library for Windows",
+        name: package_json.name,
+        version: package_json.version,
+        description: package_json.description,
         date: Temporal.Now.plainDateTimeISO(),
         hash: "a4d0cb71-cdee-48c0-bfd4-4dedbb007027",
         symbol: "🪟",
