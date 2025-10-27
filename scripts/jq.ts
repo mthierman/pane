1c0b4ae2f6112510acd97c75a6e1c5a1377f5dd5
mthierman <mthierman@gmail.com>
2025-10-27 16:42:11 -0600

    jq script

---
+++ scripts/jq.ts
@@ -0,0 +1,15 @@
+import { execSync } from "node:child_process";
+
+const git_output = execSync(`git log -5 --pretty=format:%h%x00%an%x00%aI%x00%s%x00`, {
+    encoding: "utf-8",
+});
+
+const commits = git_output
+    .split("\n")
+    .filter(Boolean)
+    .map((line) => {
+        const [commit, author, date, message] = line.split("\x00");
+        return { commit, author, date, message };
+    });
+
+console.log(JSON.stringify(commits, null, 2));
