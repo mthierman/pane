6452610d9d3dc73bcf7cf62c70c225c518d3f96e
mthierman <mthierman@gmail.com>
2025-10-26 23:07:03 -0600

    Fix example to use argc, argv

--- examples/console/main.cpp
+++ examples/console/main.cpp
@@ -1,18 +1,15 @@
 #include <pane/pane.hpp>
-#include <algorithm>
 #include <cstdlib>
 #include <print>
+#include <ranges>
 
 // https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
-auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
-    // std::println("{} v{} {} v{}",
-    //              pane::project_name,
-    //              pane::project_version,
-    //              pane::os_name,
-    //              pane::os_version);
+auto wmain(int argc, wchar_t** argv, wchar_t** /* envp */) -> int {
+    auto args { pane::system::get_argv_u8(argc, argv) };
 
-    auto args { pane::system::get_argv_u8() };
-    std::ranges::for_each(args, [idx = 0](const auto& arg) { std::println("{}: {}", idx, arg); });
+    for (auto [idx, arg] : std::views::enumerate(args)) {
+        std::println("{}: {}", idx, arg);
+    }
 
     return EXIT_SUCCESS;
 }
