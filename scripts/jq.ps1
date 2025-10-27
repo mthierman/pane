917c7692479e335f48a73deb2db4b39347a49531
mthierman <mthierman@gmail.com>
2025-10-27 09:53:25 -0600

    Restore jq script

--- scripts/jq.ps1
+++ scripts/jq.ps1
@@ -1,16 +1,14 @@
 # https://til.simonwillison.net/jq/git-log-json
 
-Get-ChildItem
+if (-not (Test-Path "build/generated"))
+{
+    New-Item -ItemType Directory -Path "build/generated" | Out-Null
+}
 
-# if (-not (Test-Path "build/generated"))
-# {
-#   New-Item -ItemType Directory -Path "build/generated" | Out-Null
-# }
-
-# git log -5 --pretty=format:'%h%x00%an%x00%aI%x00%s%x00' | `
-#     jq -R -s '[split("\n")[:-1] | map(split("\u0000")) | .[] | {
-#     "commit": .[0],
-#     "author": .[1],
-#     "date": .[2],
-#     "message": .[3]
-#   }]' > "build/generated/five_latest_commits.json"
+git log -5 --pretty=format:'%h%x00%an%x00%aI%x00%s%x00' | `
+        jq -R -s '[split("\n")[:-1] | map(split("\u0000")) | .[] | {
+    "commit": .[0],
+    "author": .[1],
+    "date": .[2],
+    "message": .[3]
+  }]' > "build/generated/five_latest_commits.json"
