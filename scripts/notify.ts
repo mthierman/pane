3d3233d0656773cc2a983b860b60606f07dec912
mthierman <mthierman@gmail.com>
2025-10-27 22:19:39 -0600

    Simplify payload

--- scripts/notify.ts
+++ scripts/notify.ts
@@ -4,25 +4,31 @@ import { spawnSync } from "node:child_process";
 import package_json from "../package.json" with { type: "json" };
 
 const client_payload = {
-    name: package_json.name,
-    version: package_json.version,
-    description: package_json.description,
-    date: Temporal.Now.plainDateTimeISO().toString(),
-    hash: spawnSync("git", ["rev-parse", "HEAD"], { encoding: "utf-8" }).stdout.trim(),
-    symbol: "🪟",
-    github: package_json.homepage,
-    repo: package_json.repository.url,
-    releases: package_json.pane.releases,
-    download: package_json.pane.releases,
-    recent_commits: spawnSync("git", ["log", "-5", "--pretty=format:%h%x00%an%x00%aI%x00%s%x00"], {
-        encoding: "utf-8",
-    })
-        .stdout.split("\n")
-        .filter(Boolean)
-        .map((line) => {
-            const [commit, author, date, message] = line.split("\x00");
-            return { commit, author, date, message };
-        }),
+    [package_json.name]: {
+        name: package_json.name,
+        version: package_json.version,
+        description: package_json.description,
+        build_time: Temporal.Now.plainDateTimeISO().toString(),
+        latest_commit: spawnSync("git", ["rev-parse", "--short", "HEAD"], {
+            encoding: "utf-8",
+        }).stdout.trim(),
+        symbol: "🪟",
+        repo: package_json.repository.url,
+        releases: package_json.pane.releases,
+        recent_commits: spawnSync(
+            "git",
+            ["log", "-5", "--pretty=format:%h%x00%an%x00%aI%x00%s%x00"],
+            {
+                encoding: "utf-8",
+            },
+        )
+            .stdout.split("\n")
+            .filter(Boolean)
+            .map((line) => {
+                const [commit, author, date, message] = line.split("\x00");
+                return { commit, author, date, message };
+            }),
+    },
 };
 
 // https://docs.github.com/en/rest/guides/scripting-with-the-rest-api-and-javascript?apiVersion=2022-11-28
