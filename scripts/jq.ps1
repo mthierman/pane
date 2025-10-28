57644d38d9db1b35ae76785d0850ba347085772e
mthierman <mthierman@gmail.com>
2025-10-27 23:46:24 -0600

    Remove jq.ps1

--- scripts/jq.ps1
+++
@@ -1,9 +0,0 @@
-# https://til.simonwillison.net/jq/git-log-json
-
-git log -5 --pretty=format:'%h%x00%an%x00%aI%x00%s%x00' | `
-        jq -R -s '[split("\n")[:-1] | map(split("\u0000")) | .[] | {
-    "commit": .[0],
-    "author": .[1],
-    "date": .[2],
-    "message": .[3]
-  }]'
