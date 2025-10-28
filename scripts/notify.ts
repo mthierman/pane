9bd336988140c743c8922c1ecfeaa7e1bef1ae8b
mthierman <mthierman@gmail.com>
2025-10-28 07:43:33 -0600

    Update notify payload

--- scripts/notify.ts
+++ scripts/notify.ts
@@ -3,31 +3,34 @@ import { Octokit } from "@octokit/rest";
 import { spawnSync } from "node:child_process";
 import package_json from "../package.json" with { type: "json" };
 
-const client_payload = {
-    [package_json.name]: {
-        name: package_json.name,
-        version: package_json.version,
-        description: package_json.description,
-        build_time: Temporal.Now.plainDateTimeISO().toString(),
-        latest_commit: spawnSync("git", ["rev-parse", "--short", "HEAD"], {
-            encoding: "utf-8",
-        }).stdout.trim(),
-        symbol: "🪟",
-        repo: package_json.repository.url,
-        releases: package_json.pane.releases,
-        recent_commits: spawnSync(
-            "git",
-            ["log", "-5", "--pretty=format:%h%x00%an%x00%aI%x00%s%x00"],
-            {
+const payload = {
+    event_type: package_json.pane.event_types.notify,
+    client_payload: {
+        [package_json.name]: {
+            name: package_json.name,
+            version: package_json.version,
+            description: package_json.description,
+            build_time: Temporal.Now.plainDateTimeISO().toString(),
+            latest_commit: spawnSync("git", ["rev-parse", "--short", "HEAD"], {
                 encoding: "utf-8",
-            },
-        )
-            .stdout.split("\n")
-            .filter(Boolean)
-            .map((line) => {
-                const [commit, author, date, message] = line.split("\x00");
-                return { commit, author, date, message };
-            }),
+            }).stdout.trim(),
+            symbol: "🪟",
+            repo: package_json.repository.url,
+            releases: package_json.pane.releases,
+            recent_commits: spawnSync(
+                "git",
+                ["log", "-5", "--pretty=format:%h%x00%an%x00%aI%x00%s%x00"],
+                {
+                    encoding: "utf-8",
+                },
+            )
+                .stdout.split("\n")
+                .filter(Boolean)
+                .map((line) => {
+                    const [commit, author, date, message] = line.split("\x00");
+                    return { commit, author, date, message };
+                }),
+        },
     },
 };
 
@@ -37,14 +40,12 @@ if (process.env.CI) {
     await octokit.repos.createDispatchEvent({
         owner: package_json.pane.owner,
         repo: package_json.pane.web_repo,
-        event_type: package_json.pane.event_types.notify,
-        client_payload: client_payload,
+        ...payload,
     });
 } else {
     console.dir(
         {
-            event_type: package_json.pane.event_types.notify,
-            client_payload: client_payload,
+            ...payload,
         },
         { depth: null },
     );
