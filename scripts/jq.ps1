e5c22bc56d86c0a5567bc56f429af628de4e72b2
mthierman <mthierman@gmail.com>
2025-10-27 21:50:56 -0600

    Simplify jq.ps1

--- scripts/jq.ps1
+++ scripts/jq.ps1
@@ -1,14 +1,9 @@
 # https://til.simonwillison.net/jq/git-log-json
 
-if (-not (Test-Path "build/generated"))
-{
-    New-Item -ItemType Directory -Path "build/generated" | Out-Null
-}
-
 git log -5 --pretty=format:'%h%x00%an%x00%aI%x00%s%x00' | `
         jq -R -s '[split("\n")[:-1] | map(split("\u0000")) | .[] | {
     "commit": .[0],
     "author": .[1],
     "date": .[2],
     "message": .[3]
-  }]' > "build/generated/five_latest_commits.json"
+  }]'
