b33a08815b00538dfbde6e1e5c0e4b1ac429dda6
mthierman <mthierman@gmail.com>
2025-10-30 19:37:10 -0600

    Regular console example

---
+++ example/console/main.cpp
@@ -0,0 +1,6 @@
+#include <pane/pane.hpp>
+
+// https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
+auto wmain(int argc, wchar_t* argv[], [[maybe_unused]] wchar_t* envp[]) -> int {
+    return 0;
+}
