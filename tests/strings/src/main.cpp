043123aa25192663753ea44d83f76d8c7f091f2c
mthierman <mthierman@gmail.com>
2025-10-30 19:34:04 -0600

    Test u16strings

--- tests/strings/src/main.cpp
+++ tests/strings/src/main.cpp
@@ -1,12 +1,16 @@
 #include <pane/pane.hpp>
 #include <print>
 
+using pane::to_utf8_lossy;
+
 auto wmain(int argc, wchar_t* argv[], [[maybe_unused]] wchar_t* envp[]) -> int {
     auto test { u8"test" };
     auto u8string { std::u8string { test } };
 
     std::println("{}", test);
     std::println("{}", u8string);
+
+    std::println("{}", to_utf8_lossy(u"test"));
 
     return 0;
 }
