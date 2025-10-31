966b552461a6976cf1cee8992bc46dd66b7008ce
mthierman <mthierman@gmail.com>
2025-10-30 19:41:47 -0600

    By ref

--- example/console/main.cpp
+++ example/console/main.cpp
@@ -5,7 +5,7 @@
 auto wmain(int argc, wchar_t* argv[], [[maybe_unused]] wchar_t* envp[]) -> int {
     auto args { pane::system::get_argv_u8(argc, argv) };
 
-    for (auto arg : args) {
+    for (auto& arg : args) {
         std::println("{}", arg);
     }
 
