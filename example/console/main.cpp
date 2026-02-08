534445d4813eea797f872504305205c9fa950b4e
mthierman <mthierman@gmail.com>
2026-02-07 21:12:44 -0700

    Fix build error in console example

--- example/console/main.cpp
+++ example/console/main.cpp
@@ -2,7 +2,7 @@
 #include <print>
 
 // https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
-auto wmain(int argc, wchar_t* argv[], [[maybe_unused]] wchar_t* envp[]) -> int {
+auto wmain(int /* argc */, wchar_t* /* argv */[], wchar_t* /* envp */[]) -> int {
     // auto args { pane::system::get_argv_u8(argc, argv) };
 
     // for (auto& arg : args) {
